c2a1ca109c91f163503a65c2d1b685f92aaac76a
Sean Welch <seanw51199@gmail.com>
2023-10-28 16:39:37 +0100

    added dependancies - tailwind & solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  integrations: [tailwind(), solidJs()]
+});
