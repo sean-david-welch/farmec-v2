edfe2f88dc0999459aa16a9a18dd3b29f1392f53
Sean Welch <seanw51199@gmail.com>
2023-10-29 11:56:46 +0000

    some inital styles

--- sst.config.ts
+++ sst.config.ts
@@ -2,14 +2,14 @@ import type { SSTConfig } from 'sst';
 import { AstroSite } from 'sst/constructs';
 
 export default {
-  config(_input: any) {
+  config(_input) {
     return {
       name: 'farmec-astro',
       region: 'eu-west-1',
     };
   },
-  stacks(app: any) {
-    app.stack(function Site({ stack }: any) {
+  stacks(app) {
+    app.stack(function Site({ stack }) {
       const site = new AstroSite(stack, 'site');
       stack.addOutputs({
         url: site.url,
