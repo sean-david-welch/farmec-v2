c9fcb4ab4dc346591dcabe9e1be47396fef74fc5
Sean Welch <seanw51199@gmail.com>
2023-10-29 10:57:17 +0000

    get supplier by id

---
+++ src/types/supplier.ts
@@ -0,0 +1,13 @@
+export default interface Supplier {
+  id: string;
+  name: string;
+  description: string;
+  logo_image: string;
+  marketing_image: string;
+  social_facebook: string;
+  social_instagram: string;
+  social_linkedin: string;
+  social_twitter: string;
+  social_youtube: string;
+  social_website: string;
+}
