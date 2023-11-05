8924f6a0273138a0e926d1cee02e547a26e266df
Sean Welch <seanw51199@gmail.com>
2023-11-05 15:35:46 +0000

    got nav drop working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      md: { max: "896px" },
+      lg: { min: "896px" },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
