b4806a45e641a5ed9e8de9ce165bad2d3fd238f6
Sean Welch <seanwelch@Seans-Laptop.local>
2023-11-15 20:22:11 +0000

    switch back to node ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-
 import deno from '@astrojs/deno';
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: deno(),
-  integrations: [solidJs(), tailwind()],
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [solidJs(), tailwind()]
 });
