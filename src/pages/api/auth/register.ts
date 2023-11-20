66067a67afb787c832cda1e50cc24b0756061a75
Sean Welch <seanwelch@Seans-Laptop.local>
2023-11-20 08:33:47 +0000

    firebase astro

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,16 +1,24 @@
 import type { APIRoute } from 'astro';
 
 import { getAuth } from 'firebase-admin/auth';
-import { pool } from '../../database/connection';
+import { pool } from '../../../database/connection';
 
 export const POST: APIRoute = async ({ request }): Promise<Response> => {
   const client = await pool.connect();
 
   try {
-    const data = await request.json();
-    const { email, password, role } = data;
+    const formData = await request.formData();
 
-    const userRecord = await getAuth().createUser({ email, password });
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+    const name = formData.get('name')?.toString();
+    const role = formData.get('role')?.toString();
+
+    if (!email || !password || !name) {
+      return new Response('Missing form data', { status: 400 });
+    }
+
+    const userRecord = await getAuth().createUser({ email, password, displayName: name });
 
     const sql = `INSERT INTO users(uid, email, role) VALUES($1, $2, $3) RETURNING *`;
 
