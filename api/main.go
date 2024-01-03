f93ee4a7e1eb712fac46adce22a84080b765842e
Sean Welch <seanwelch51199@icloud.com>
2024-01-03 16:29:35 +0000

    structs and router

--- api/main.go
+++ api/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	router := gin.Default()
+
+	router.GET("/suppliers")
+
 	fmt.Println("Hello World!")
 }
