1c92091b5b3c7d441221ae29a1f3f2403a2a7207
Sean Welch <seanwelch51199@icloud.com>
2024-01-04 10:15:47 +0000

    route and db connection

--- api/main.go
+++ api/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"os"
 
+	"githib.com/sean-david-welch/Farmec-Astro/api/controllers"
+	"githib.com/sean-david-welch/Farmec-Astro/api/routes"
+	"githib.com/sean-david-welch/Farmec-Astro/api/services"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+        log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
 
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 	router.Use(cors.Default())
 
-	router.Run(":8080")
+	supplierService := services.NewSupplierService(db)
+	supplierController := controllers.NewSuppliersContoller(supplierService)
+	routes.SupplierRoutes(router, supplierController)
 
-	fmt.Println("Server Running on Port 8080")
+	router.Run(":8080")
 }
