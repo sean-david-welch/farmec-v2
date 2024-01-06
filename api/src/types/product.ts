c8bd0bda53a8d1fdfb7c3d4f3a518215082b0f0d
Sean Welch <seanwelch51199@icloud.com>
2024-01-06 17:00:58 +0000

    remove fastify api

--- api/src/types/product.ts
+++
@@ -1,8 +0,0 @@
-export default interface Product {
-  id: string;
-  machineId: string;
-  name: string;
-  product_image: string;
-  description: string;
-  product_link: string;
-}
--- api/src/types/sparepart.ts
+++
@@ -1,8 +0,0 @@
-export default interface Sparepart {
-  id: string;
-  supplierId: string;
-  name: string;
-  parts_image: string;
-  spare_parts_link: string;
-  pdf_link: string;
-}
