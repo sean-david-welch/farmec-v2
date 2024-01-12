d3728f4a0d034e63a916ecd200839979f96fc16a
Sean Welch <seanwelch51199@icloud.com>
2024-01-12 16:09:13 +0000

    privacy and term controllers

---
+++ server/controllers/privacyController.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sean-david-welch/farmec-v2/server/models"
+	"github.com/sean-david-welch/farmec-v2/server/services"
+)
+
+type PrivacyController struct {
+	service *services.PrivacyService
+}
+
+func NewPrivacyController(service *services.PrivacyService) *PrivacyController {
+	return &PrivacyController{service: service}
+}
+
+func(controller *PrivacyController) GetPrivacys(context *gin.Context) {
+	privacys, err := controller.service.GetPrivacys(); if err != nil {
+		log.Printf("error getting privacys: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while getting privacy"})
+		return
+	}
+
+	context.JSON(http.StatusOK, privacys)
+} 
+
+func(controller *PrivacyController) CreatePrivacy(context *gin.Context) {
+	var privacy models.Privacy
+
+	if err := context.ShouldBindJSON(&privacy); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
+		return
+	}
+
+	if err := controller.service.CreatePrivacy(&privacy); err != nil {
+		log.Printf("error while creating privacy: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating privacy", "details": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, privacy)
+}
+
+func(controller *PrivacyController) UpdatePrivacy(context *gin.Context) {
+	id := context.Param("id")
+	var privacy models.Privacy
+
+	if err := context.ShouldBindJSON(&privacy); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
+		return
+	}
+
+	if err := controller.service.UpdatePrivacy(id, &privacy); err != nil {
+		log.Printf("error while updating privacy: %v", err)
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Error occurred while updating privacy", "details": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusAccepted, privacy)
+}
+
+func(controller *PrivacyController) DeletePrivacy(context *gin.Context) {
+	id := context.Param("id")
+
+	if err := controller.service.DeletePrivacy(id); err != nil {
+		log.Printf("Error deleting privacy: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while deleting privacy", "details": err.Error()})
+		return 
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "privacy deleted successfully", "id": id})
+}
---
+++ server/controllers/termsController.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sean-david-welch/farmec-v2/server/models"
+	"github.com/sean-david-welch/farmec-v2/server/services"
+)
+
+type TermsController struct {
+	service *services.TermService
+}
+
+func NewTermsController(service *services.TermService) *TermsController {
+	return &TermsController{service: service}
+}
+
+func(controller *TermsController) GetTerms(context *gin.Context) {
+	terms, err := controller.service.GetTerms(); if err != nil {
+		log.Printf("error getting terms: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while getting term"})
+		return
+	}
+
+	context.JSON(http.StatusOK, terms)
+} 
+
+func(controller *TermsController) CreateTerm(context *gin.Context) {
+	var term models.Terms
+
+	if err := context.ShouldBindJSON(&term); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
+		return
+	}
+
+	if err := controller.service.CreateTerm(&term); err != nil {
+		log.Printf("error while creating term: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating term", "details": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, term)
+}
+
+func(controller *TermsController) UpdateTerm(context *gin.Context) {
+	id := context.Param("id")
+	var term models.Terms
+
+	if err := context.ShouldBindJSON(&term); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
+		return
+	}
+
+	if err := controller.service.UpdateTerm(id, &term); err != nil {
+		log.Printf("error while updating term: %v", err)
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Error occurred while updating term", "details": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusAccepted, term)
+}
+
+func(controller *TermsController) DeleteTerm(context *gin.Context) {
+	id := context.Param("id")
+
+	if err := controller.service.DeleteTerm(id); err != nil {
+		log.Printf("Error deleting term: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while deleting term", "details": err.Error()})
+		return 
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "term deleted successfully", "id": id})
+}
