b639d2b49321d8388aa3a8a18560d32c279a064d
Sean Welch <seanwelch51199@icloud.com>
2024-01-21 13:23:24 +0000

    privacy repo test

--- server/repository/privacyRepository.go
+++ server/repository/privacyRepository.go
@@ -10,10 +10,10 @@ import (
 )
 
 type PrivacyRepository interface {
-	GetPrivacy() ([]types.Privacy, error) 
-	CreatePrivacy(privacy *types.Privacy) error 
-	UpdatePrivacy(id string, privacy *types.Privacy) error 
-	DeletePrivacy(id string) error 
+	GetPrivacy() ([]types.Privacy, error)
+	CreatePrivacy(privacy *types.Privacy) error
+	UpdatePrivacy(id string, privacy *types.Privacy) error
+	DeletePrivacy(id string) error
 }
 
 type PrivacyRepositoryImpl struct {
@@ -24,11 +24,12 @@ func NewPrivacyRepository(database *sql.DB) *PrivacyRepositoryImpl {
 	return &PrivacyRepositoryImpl{database: database}
 }
 
-func(repository *PrivacyRepositoryImpl) GetPrivacy() ([]types.Privacy, error) {
+func (repository *PrivacyRepositoryImpl) GetPrivacy() ([]types.Privacy, error) {
 	var privacyTerms []types.Privacy
 
 	query := `SELECT * FROM "Privacy"`
-	rows, err := repository.database.Query(query); if err != nil {
+	rows, err := repository.database.Query(query)
+	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
@@ -36,7 +37,7 @@ func(repository *PrivacyRepositoryImpl) GetPrivacy() ([]types.Privacy, error) {
 	for rows.Next() {
 		var privacyTerm types.Privacy
 
-		if err := rows.Scan(&privacyTerm.ID, privacyTerm.Title, privacyTerm.Body, privacyTerm.Created); err != nil {
+		if err := rows.Scan(&privacyTerm.ID, &privacyTerm.Title, &privacyTerm.Body, &privacyTerm.Created); err != nil {
 			return nil, fmt.Errorf("error occurred while scanning row: %w", err)
 		}
 
@@ -44,39 +45,42 @@ func(repository *PrivacyRepositoryImpl) GetPrivacy() ([]types.Privacy, error) {
 
 		if err := rows.Err(); err != nil {
 			return nil, fmt.Errorf("error occurred after iterating over rows: %w", err)
-		}	
+		}
 	}
 
 	return privacyTerms, err
 }
 
-func(repository *PrivacyRepositoryImpl) CreatePrivacy(privacy *types.Privacy) error {
+func (repository *PrivacyRepositoryImpl) CreatePrivacy(privacy *types.Privacy) error {
 	privacy.ID = uuid.NewString()
 	privacy.Created = time.Now()
 
 	query := `INSERT INTO "Privacy" (id, title, body, created) VALUES ($1, $2, $3, $4)`
 
-	_, err := repository.database.Exec(query, privacy.ID, privacy.Title, privacy.Body, privacy.Created); if err != nil {
+	_, err := repository.database.Exec(query, privacy.ID, privacy.Title, privacy.Body, privacy.Created)
+	if err != nil {
 		return fmt.Errorf("error occurred while creating privacy term: %w", err)
 	}
-	
+
 	return nil
 }
 
-func(repository *PrivacyRepositoryImpl) UpdatePrivacy(id string, privacy *types.Privacy) error {
+func (repository *PrivacyRepositoryImpl) UpdatePrivacy(id string, privacy *types.Privacy) error {
 	query := `UPDATE "Privacy" SET title = $1, body = $2 where id = $3`
 
-	_, err := repository.database.Exec(query, privacy.Title, privacy.Body, id); if err != nil {
+	_, err := repository.database.Exec(query, privacy.Title, privacy.Body, id)
+	if err != nil {
 		return fmt.Errorf("error occurred while updating privacy term: %w", err)
 	}
 
 	return nil
 }
 
-func(repository *PrivacyRepositoryImpl) DeletePrivacy(id string) error {
+func (repository *PrivacyRepositoryImpl) DeletePrivacy(id string) error {
 	query := `DELETE FROM "Privacy" WHERE "id" = $1`
 
-	_, err := repository.database.Exec(query, id); if err != nil {
+	_, err := repository.database.Exec(query, id)
+	if err != nil {
 		return fmt.Errorf("error occurred while deleting privacy term: %w", err)
 	}
 
---
+++ server/tests/repositories/privacyRepository_test.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/sean-david-welch/farmec-v2/server/repository"
+	"github.com/sean-david-welch/farmec-v2/server/tests/mocks"
+	"github.com/sean-david-welch/farmec-v2/server/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPrivacy(test *testing.T) {
+	db, mock, err := mocks.InitMockDatabase(test)
+	if err != nil {
+		test.Fatalf("Failed to init mock database: %s", err)
+	}
+	defer db.Close()
+
+	privacys := []types.Privacy{
+		{
+			ID:      "1",
+			Title:   "Title 1",
+			Body:    "Body 1",
+			Created: time.Now(),
+		},
+		{
+			ID:      "2",
+			Title:   "Title 2",
+			Body:    "Body 2",
+			Created: time.Now(),
+		},
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "title", "body", "created"})
+	for _, privacy := range privacys {
+		rows.AddRow(privacy.ID, privacy.Title, privacy.Body, privacy.Created)
+	}
+
+	mock.ExpectQuery(`SELECT \* FROM "Privacy"`).WillReturnRows(rows)
+
+	repo := repository.NewPrivacyRepository(db)
+	retrieved, err := repo.GetPrivacy()
+	if err != nil {
+		test.Fatalf("error occurred while getting items: %s", err)
+	}
+
+	assert.NoError(test, err)
+	if err == nil {
+		assert.Len(test, retrieved, len(privacys))
+		assert.Equal(test, privacys, retrieved)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		test.Errorf("unfullfilled expectations: %s", err)
+	}
+}
+
+func TestCreatePrivacy(test *testing.T) {
+	db, mock, err := mocks.InitMockDatabase(test)
+	if err != nil {
+		test.Fatalf("Failed to init mock database: %s", err)
+	}
+	defer db.Close()
+
+	privacy := &types.Privacy{
+		ID:      "1",
+		Title:   "Title 1",
+		Body:    "Body 1",
+		Created: time.Now(),
+	}
+
+	mock.ExpectExec(`INSERT INTO "Privacy" \(id, title, body, created\) VALUES \(\$1, \$2, \$3, \$4\)`).
+		WithArgs(sqlmock.AnyArg(), privacy.Title, privacy.Body, sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	repo := repository.NewPrivacyRepository(db)
+	err = repo.CreatePrivacy(privacy)
+
+	assert.NoError(test, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		test.Errorf("unfullfilled expectations: %s", err)
+	}
+}
