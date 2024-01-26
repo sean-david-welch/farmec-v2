10a146eec356167dea5a48c33ecddf540768f99f
Sean Welch <seanwelch51199@icloud.com>
2024-01-26 08:50:06 +0000

    downloaded assets

--- client/src/utils/env.ts
+++ client/src/utils/env.ts
@@ -1,14 +1,14 @@
 const config = {
-    baseUrl: import.meta.env.BASE_URL,
+    baseUrl: import.meta.env.VITE_BASE_URL,
 
-    firebaseApiKey: import.meta.env.FB_WEB_API_KEY,
-    firebaseAuthDomain: import.meta.env.FB_AUTH_URL,
-    firebaseProjectId: import.meta.env.FB_PROJECT_ID,
+    firebaseApiKey: import.meta.env.VITE_FB_WEB_API_KEY,
+    firebaseAuthDomain: import.meta.env.VITE_FB_AUTH_URL,
+    firebaseProjectId: import.meta.env.VITE_FB_PROJECT_ID,
 
-    mapsKey: import.meta.env.PUBLIC_MAPS_KEY,
-    recaptchaKey: import.meta.env.RECAPTCHA_PUBLIC_KEY,
+    mapsKey: import.meta.env.VITE_PUBLIC_MAPS_KEY,
+    recaptchaKey: import.meta.env.VITE_RECAPTCHA_PUBLIC_KEY,
 
-    stripePublicKey: import.meta.env.STRIPE_PUBLIC_KEY,
+    stripePublicKey: import.meta.env.VITE_STRIPE_PUBLIC_KEY,
 };
 
 export default config;
