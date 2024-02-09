2537cedec1277e6997e8507c083112b3bc12ed0d
Sean Welch <seanwelch51199@icloud.com>
2024-02-09 18:09:48 +0000

    tried to fix contact form tcp error

--- client/src/forms/ContactForm.tsx
+++ client/src/forms/ContactForm.tsx
@@ -1,48 +1,75 @@
 import utils from '../styles/Utils.module.css';
 import styles from '../styles/Home.module.css';
 import config from '../lib/env';
+import { useState } from 'react';
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
     const url = new URL('/api/contact', config.baseUrl);
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const formData = new FormData(event.currentTarget);
 
         const body = {
-            name: formData.get('name'),
-            email: formData.get('email'),
-            message: formData.get('message'),
+            name: name,
+            email: email,
+            message: message,
         };
 
-        // Add error handling for the fetch request as needed
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
+        try {
+            const repsonse = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
 
-        // Handle response (e.g., showing a success message)
+            if (repsonse.ok) {
+                setName('');
+                setEmail('');
+                setMessage('');
+            }
+        } catch (error) {
+            console.error('an error occurred', error);
+        }
     };
 
     return (
         <form onSubmit={handleSubmit} className={styles.contactForm}>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" required={true} placeholder="name" />
+            <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                placeholder="Your name"
+            />
             <label htmlFor="email">Email:</label>
-            <input type="email" name="email" required={true} placeholder="email" />
+            <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Your email"
+            />
             <label htmlFor="message">Message:</label>
             <textarea
+                id="message"
                 name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 placeholder="Enter your message here..."
                 cols={30}
                 rows={11}
-                required={true}
+                required
             />
-
-            <div className={styles.recaptcha}></div>
 
             <button className={utils.btnForm} type="submit">
                 Submit
