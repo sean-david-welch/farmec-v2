6c5c59f33a6b463a0690aeb3ad12ee4bc403f477
Sean Welch <seanwelch51199@icloud.com>
2024-02-19 20:33:41 +0000

    sticky nav on mobile

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,37 +1,15 @@
 import styles from '../styles/Home.module.css';
 import config from '../lib/env';
 
-import { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 export const Map: React.FC = () => {
     const mapsKey = config.mapsKey;
 
-    const [containerStyle, setContainerStyle] = useState({
+    const containerStyle = {
         width: '600px',
         height: '600px',
-    });
-
-    useEffect(() => {
-        function handleResize() {
-            if (window.innerWidth < 768) {
-                setContainerStyle({
-                    width: '100%',
-                    height: '400px',
-                });
-            } else {
-                setContainerStyle({
-                    width: '600px',
-                    height: '600px',
-                });
-            }
-        }
-
-        window.addEventListener('resize', handleResize);
-        handleResize();
-
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+    };
 
     const location = {
         lat: 53.49200990196934,
@@ -45,7 +23,7 @@ export const Map: React.FC = () => {
     return (
         <div className={styles.map}>
             <LoadScript googleMapsApiKey={mapsKey}>
-                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
+                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
                     <Marker position={location} />
                 </GoogleMap>
             </LoadScript>
