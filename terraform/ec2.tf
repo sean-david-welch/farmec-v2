049e0d5b934272ab519ee6267b95a6f5c8e844b7
Sean Welch <seanwelch51199@icloud.com>
2024-02-23 17:40:02 +0000

    some more config

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "FarmecAPI" {
   ami           = "ami-0905a3c97561e0b69"
-  instance_type = "t3.nano"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.farmec_subnet.id  
   key_name = aws_key_pair.farmec-ec2.key_name
   vpc_security_group_ids = [aws_security_group.my_ec2_sg.id]  
