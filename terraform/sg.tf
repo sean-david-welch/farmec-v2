d0f7bedeaf780564020cca557024bf143ca991e9
Sean Welch <seanwelch51199@icloud.com>
2024-02-25 11:54:16 +0000

    some more terraform

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -38,6 +38,14 @@ resource "aws_security_group" "my_ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
