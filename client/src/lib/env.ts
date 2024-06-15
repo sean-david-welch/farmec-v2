207e8903cc3a7d23d8e7a6a04a11b013aaa16386
seanwelch <seanwelch51199@icloud.com>
2024-06-15 16:54:04 +0100

    some changes to env

--- client/src/lib/env.ts
+++ client/src/lib/env.ts
@@ -1,15 +1,15 @@
 const config = {
-    baseUrl: import.meta.env.VITE_ENVIRONMENT === 'production' ? 'https://server.farmec.ie/' : 'http://0.0.0.0:80/',
+	baseUrl: import.meta.env.VITE_ENVIRONMENT === 'production' ? 'https://server.farmec.ie/' : 'http://127.0.0.1:8000/',
 
-    firebaseApiKey: import.meta.env.VITE_FB_WEB_API_KEY,
-    firebaseAuthDomain: import.meta.env.VITE_FB_AUTH_URL,
-    firebaseProjectId: import.meta.env.VITE_FB_PROJECT_ID,
+	firebaseApiKey: import.meta.env.VITE_FB_WEB_API_KEY,
+	firebaseAuthDomain: import.meta.env.VITE_FB_AUTH_URL,
+	firebaseProjectId: import.meta.env.VITE_FB_PROJECT_ID,
 
-    mapsKey: import.meta.env.VITE_PUBLIC_MAPS_KEY,
-    recaptchaKey: import.meta.env.VITE_RECAPTCHA_PUBLIC_KEY,
+	mapsKey: import.meta.env.VITE_PUBLIC_MAPS_KEY,
+	recaptchaKey: import.meta.env.VITE_RECAPTCHA_PUBLIC_KEY,
 
-    stripePublicKey: import.meta.env.VITE_STRIPE_PUBLIC_KEY,
-    stripePublicKeyTest: import.meta.env.VITE_TEST_PUBLIC_KEY,
+	stripePublicKey: import.meta.env.VITE_STRIPE_PUBLIC_KEY,
+	stripePublicKeyTest: import.meta.env.VITE_TEST_PUBLIC_KEY,
 };
 
 export default config;
