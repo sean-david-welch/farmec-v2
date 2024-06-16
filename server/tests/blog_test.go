1f439d616e1767d6d1ffd2c5adeaf266f56c0492
seanwelch <seanwelch51199@icloud.com>
2024-06-16 12:17:49 +0100

    quick change for rebuild

--- server/tests/blog_test.go
+++ server/tests/blog_test.go
@@ -36,8 +36,7 @@ func setupTestDB() (*sql.DB, error) {
 		Subheading TEXT,
 		Body TEXT,
 		Created DATETIME
-	);
-	`
+	);`
 	_, err = db.Exec(schema)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create schema: %w", err)
