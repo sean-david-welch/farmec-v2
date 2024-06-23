0931e2054fca0cfd68c91903cb0fbd777c67aaab
seanwelch <seanwelch51199@icloud.com>
2024-06-23 10:24:39 +0100

    queries for blog

--- server/sql/queries/blogs.sql
+++ server/sql/queries/blogs.sql
@@ -2,3 +2,27 @@
 SELECT id, title, date, main_image, subheading, body, created
 FROM Blog
 ORDER BY created DESC;
+
+-- name: GetBlogByID :one
+SELECT *
+FROM Blog
+WHERE id = ?;
+
+-- name: CreateBlog :exec
+INSERT into Blog
+(id, title, date, main_image, subheading, body, created)
+VALUES (?, ?, ?, ?, ?, ?, ?);
+
+-- name: UpdateBlogNoImage :exec
+UPDATE Blog
+SET title = ?, date = ?, subheading = ?, body = ?
+WHERE id = ?;
+
+-- name: UpdateBlog :exec
+UPDATE Blog
+SET title = ?, date = ?, main_image = ?, subheading = ?, body = ?
+WHERE id = ?;
+
+-- name: DeleteBlog :exec
+DELETE from Blog
+WHERE id = ?
