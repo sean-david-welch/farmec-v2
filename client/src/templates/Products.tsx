02af985c189c7ce8d36e1d86b6e040b140e91e37
seanwelch <seanwelch51199@icloud.com>
2024-07-10 20:53:30 +0100

    products image error

--- client/src/templates/Products.tsx
+++ client/src/templates/Products.tsx
@@ -9,47 +9,52 @@ import { Machine, Product } from '../types/supplierTypes';
 import { useGetResourceById } from '../hooks/genericHooks';
 
 interface Props {
-    id: string;
-    isAdmin: boolean;
-    products: Product[];
+	id: string;
+	isAdmin: boolean;
+	products: Product[];
 }
 
 const Products: React.FC<Props> = ({ id, isAdmin, products }: Props) => {
-    const { data: machine } = useGetResourceById<Machine>('machines', id);
+	const { data: machine } = useGetResourceById<Machine>('machines', id);
 
-    if (!machine) {
-        return <Loading />;
-    }
+	if (!machine) {
+		return <Loading />;
+	}
 
-    return (
-        <section id="products">
-            <div className={styles.productGrid}>
-                {products.map(product => (
-                    <div className={styles.productCard} key={product.id} id={product.name || ''}>
-                        <h1 className={utils.mainHeading}>{product.name}</h1>
-                        <a href={product.product_link || '#'} target="_blank">
-                            <img
-                                src={product.product_image || '/default.jpg'}
-                                alt={'/default.jpg'}
-                                className={styles.productImage}
-                                width={500}
-                                height={500}
-                            />
-                        </a>
-                        <p className={utils.paragraph}>{product.description}</p>
+	const imageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+		event.currentTarget.src = '/default.jpg';
+	};
 
-                        {isAdmin && product.id && (
-                            <div className={utils.optionsBtn}>
-                                <ProductForm id={product.id} product={product} machine={machine} />
-                                <DeleteButton id={product.id} resourceKey="products" />
-                            </div>
-                        )}
-                    </div>
-                ))}
-            </div>
-            {isAdmin && <ProductForm machine={machine} />}
-        </section>
-    );
+	return (
+		<section id="products">
+			<div className={styles.productGrid}>
+				{products.map(product => (
+					<div className={styles.productCard} key={product.id} id={product.name || ''}>
+						<h1 className={utils.mainHeading}>{product.name}</h1>
+						<a href={product.product_link || '#'} target="_blank">
+							<img
+								src={product.product_image}
+								alt={'/default.jpg'}
+								className={styles.productImage}
+								width={500}
+								height={500}
+								onError={imageError}
+							/>
+						</a>
+						<p className={utils.paragraph}>{product.description}</p>
+
+						{isAdmin && product.id && (
+							<div className={utils.optionsBtn}>
+								<ProductForm id={product.id} product={product} machine={machine} />
+								<DeleteButton id={product.id} resourceKey="products" />
+							</div>
+						)}
+					</div>
+				))}
+			</div>
+			{isAdmin && <ProductForm machine={machine} />}
+		</section>
+	);
 };
 
 export default Products;
