afd9d605dbc2bfc1026bd8afde9e9d5548f4739a
seanwelch <seanwelch51199@icloud.com>
2024-07-10 20:54:12 +0100

    carousel image error

--- client/src/templates/Carousel.tsx
+++ client/src/templates/Carousel.tsx
@@ -10,65 +10,70 @@ import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
 import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons/faChevronCircleDown';
 
 interface ImageProps {
-    images: string[];
+	images: string[];
 }
 
 const Carousel = (props: ImageProps) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-    const nextStep = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === props.images.length - 1 ? 0 : prevIndex + 1));
-    };
+	const nextStep = () => {
+		setCurrentIndex(prevIndex => (prevIndex === props.images.length - 1 ? 0 : prevIndex + 1));
+	};
 
-    const prevStep = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? props.images.length - 1 : prevIndex - 1));
-    };
+	const prevStep = () => {
+		setCurrentIndex(prevIndex => (prevIndex === 0 ? props.images.length - 1 : prevIndex - 1));
+	};
 
-    return (
-        <section id="Hero">
-            <div className={styles.heroContainer}>
-                <div className={styles.slideshow}>
-                    {props.images.map((image, index) => (
-                        <img
-                            key={index}
-                            src={image}
-                            alt="Slide"
-                            className={`${styles.slides} ${currentIndex === index ? styles.fadeIn : styles.fadeOut}`}
-                        />
-                    ))}
-                    <button className={styles.prevButton} onClick={prevStep} aria-label="last slide">
-                        <FontAwesomeIcon icon={faChevronLeft} />
-                    </button>
-                    <button className={styles.nextButton} onClick={nextStep} aria-label="next slide">
-                        <FontAwesomeIcon icon={faChevronRight} />
-                    </button>
-                </div>
-            </div>
-            <div className={styles.typewriter}>
-                <h1>
-                    <TypewriterComponent
-                        options={{
-                            loop: false,
-                            cursor: '',
-                            delay: 50,
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter
-                                .stop()
-                                .typeString('Importers & Distributors of Quality Agricultural Machinery')
-                                .start();
-                        }}
-                    />
-                </h1>
+	const imageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+		event.currentTarget.src = '/default.jpg';
+	};
 
-                <button className={utils.btn}>
-                    <a href="#Info">
-                        Find Out More: <FontAwesomeIcon icon={faChevronCircleDown} />
-                    </a>
-                </button>
-            </div>
-        </section>
-    );
+	return (
+		<section id="Hero">
+			<div className={styles.heroContainer}>
+				<div className={styles.slideshow}>
+					{props.images.map((image, index) => (
+						<img
+							key={index}
+							src={image}
+							alt="Slide"
+							onError={image}
+							className={`${styles.slides} ${currentIndex === index ? styles.fadeIn : styles.fadeOut}`}
+						/>
+					))}
+					<button className={styles.prevButton} onClick={prevStep} aria-label="last slide">
+						<FontAwesomeIcon icon={faChevronLeft} />
+					</button>
+					<button className={styles.nextButton} onClick={nextStep} aria-label="next slide">
+						<FontAwesomeIcon icon={faChevronRight} />
+					</button>
+				</div>
+			</div>
+			<div className={styles.typewriter}>
+				<h1>
+					<TypewriterComponent
+						options={{
+							loop: false,
+							cursor: '',
+							delay: 50,
+						}}
+						onInit={typewriter => {
+							typewriter
+								.stop()
+								.typeString('Importers & Distributors of Quality Agricultural Machinery')
+								.start();
+						}}
+					/>
+				</h1>
+
+				<button className={utils.btn}>
+					<a href="#Info">
+						Find Out More: <FontAwesomeIcon icon={faChevronCircleDown} />
+					</a>
+				</button>
+			</div>
+		</section>
+	);
 };
 
 export default Carousel;
