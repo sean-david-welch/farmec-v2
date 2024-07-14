aa43bab8dd691d27892272d92169f2a3740acd5a
seanwelch <seanwelch51199@icloud.com>
2024-07-14 17:05:05 +0100

    carousel queries

---
+++ server/sql/queries/carousel.sql
@@ -0,0 +1,14 @@
+--name: GetCarousels :many
+select id, name, image, created from Carousel order by created desc;
+
+-- name: GetCarouselByID :one
+select id, name, image, created from Carousel where id = ?;
+
+-- name: CreateCarousel :exec
+insert into Carousel (id, name, image, created) VALUES (?, ?, ?, ?);
+
+-- name: UpdateCarousel :exec
+update Carousel set name = ?, image = ? where id = ?;
+
+-- name: DeleteCarousel :exec
+delete from Carousel where id = ?;
