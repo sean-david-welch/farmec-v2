ea5d6fed7f2c44c24d7a99fa59ce2b5e8eb083b7
seanwelch <seanwelch51199@icloud.com>
2024-07-14 17:26:51 +0100

    exhibition sql queries and gen code

---
+++ server/sql/queries/exhibition.sql
@@ -0,0 +1,16 @@
+-- name: GetExhibitions :many
+select id, title, date, location, info, created from Exhibition order by created desc;
+
+-- name: GetExhibitionByID :one
+select id, title, date, location, info, created from Exhibition where id = ?;
+
+-- name: CreateExhibition :exec
+insert into Exhibition (id, title, date, location, info, created) values (?, ?, ?, ?, ?, ?);
+
+-- name: UpdateExhibition :exec
+update Exhibition
+set title = ?, date = ?, location = ?, info = ?
+where id = ?;
+
+-- name: DeleteExhibition :exec
+delete from Exhibition where id = ?;
