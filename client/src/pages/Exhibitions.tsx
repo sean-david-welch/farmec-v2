aff47712e365b3f32a728eeed8386063ed501816
seanwelch <seanwelch51199@icloud.com>
2024-08-25 17:01:06 +0100

    seo for exhibiions

--- client/src/pages/Exhibitions.tsx
+++ client/src/pages/Exhibitions.tsx
@@ -9,8 +9,10 @@ import ErrorPage from '../layouts/Error';
 import Loading from '../layouts/Loading';
 import ExhibitionForm from '../forms/ExhibitionForm';
 import DeleteButton from '../components/DeleteButton';
+import { Helmet } from 'react-helmet';
+import {FC} from "react";
 
-const Exhibitions: React.FC = () => {
+const Exhibitions: FC = () => {
 	const { isAdmin } = useUserStore();
 	const { data: exhibitions, isLoading, isError } = useGetResource<Exhibition[]>('exhibitions');
 
@@ -18,30 +20,50 @@ const Exhibitions: React.FC = () => {
 	if (isLoading) return <Loading />;
 
 	return (
-		<section id="exhibitions">
-			<h1 className={utils.sectionHeading}>Exhibitions:</h1>
-			<h1 className={utils.subHeading}>Check out upcoming events related to Farmec</h1>
-			{isAdmin && <ExhibitionForm />}
+		<>
+			<Helmet>
+				<title>Exhibitions - Farmec Ireland</title>
+				<meta name="description" content="Check out the upcoming exhibitions where Farmec will showcase its latest machinery. Don't miss the chance to see our products in action." />
 
-			{exhibitions && (
-				<div className={styles.exhibitions}>
-					{exhibitions.map(exhibition => (
-						<div className={styles.exhibition} key={exhibition.id}>
-							<h1 className={utils.mainHeading}>{exhibition.title}</h1>
-							<p className={utils.paragraph}>{exhibition.date}</p>
-							<p className={utils.paragraph}>{exhibition.location}</p>
-							<p className={utils.paragraph}>{exhibition.info}</p>
-							{isAdmin && exhibition.id && (
-								<div className={utils.optionsBtn}>
-									<ExhibitionForm id={exhibition.id} exhibition={exhibition} />
-									<DeleteButton id={exhibition.id} resourceKey="exhibitions" />
-								</div>
-							)}
-						</div>
-					))}
-				</div>
-			)}
-		</section>
+				<meta property="og:title" content="Exhibitions - Farmec Ireland" />
+				<meta property="og:description" content="Check out the upcoming exhibitions where Farmec will showcase its latest machinery. Don't miss the chance to see our products in action." />
+				<meta property="og:image" content="https://www.farmec.ie/farmec_images/Suppliers/sip1250.webp" />
+				<meta property="og:url" content="https://www.farmec.ie/exhibitions" />
+				<meta property="og:type" content="website" />
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="Exhibitions - Farmec Ireland" />
+				<meta name="twitter:description" content="Check out the upcoming exhibitions where Farmec will showcase its latest machinery. Don't miss the chance to see our products in action." />
+				<meta name="twitter:image" content="https://www.farmec.ie/farmec_images/Suppliers/sip1250.webp" />
+
+				<link rel="canonical" href="https://www.farmec.ie/exhibitions" />
+			</Helmet>
+
+			<section id="exhibitions">
+				<h1 className={utils.sectionHeading}>Exhibitions:</h1>
+				<h1 className={utils.subHeading}>Check out upcoming events related to Farmec</h1>
+				{isAdmin && <ExhibitionForm />}
+
+				{exhibitions && (
+					<div className={styles.exhibitions}>
+						{exhibitions.map(exhibition => (
+							<div className={styles.exhibition} key={exhibition.id}>
+								<h1 className={utils.mainHeading}>{exhibition.title}</h1>
+								<p className={utils.paragraph}>{exhibition.date}</p>
+								<p className={utils.paragraph}>{exhibition.location}</p>
+								<p className={utils.paragraph}>{exhibition.info}</p>
+								{isAdmin && exhibition.id && (
+									<div className={utils.optionsBtn}>
+										<ExhibitionForm id={exhibition.id} exhibition={exhibition} />
+										<DeleteButton id={exhibition.id} resourceKey="exhibitions" />
+									</div>
+								)}
+							</div>
+						))}
+					</div>
+				)}
+			</section>
+		</>
 	);
 };
 
