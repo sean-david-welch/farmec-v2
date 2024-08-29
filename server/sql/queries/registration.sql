00656804a0f61d0df19a58f7014ed235ec89e2be
seanwelch <seanwelch51199@icloud.com>
2024-08-29 20:17:48 +0100

    registration queries

--- server/sql/queries/registration.sql
+++ server/sql/queries/registration.sql
@@ -18,3 +18,57 @@ select id,
        completed_by,
        created
 from MachineRegistration;
+
+-- name: GetRegistrationsByID :one
+select id,
+       dealer_name,
+       dealer_address,
+       owner_name,
+       owner_address,
+       machine_model,
+       serial_number,
+       install_date,
+       invoice_number,
+       complete_supply,
+       pdi_complete,
+       pto_correct,
+       machine_test_run,
+       safety_induction,
+       operator_handbook,
+       date,
+       completed_by,
+       created
+from MachineRegistration
+where id = ?;
+
+-- name: CreateRegistration :exec
+insert into MachineRegistration (id, dealer_name, dealer_address, owner_name, owner_address, machine_model,
+                                 serial_number, install_date, invoice_number, complete_supply, pdi_complete,
+                                 pto_correct, machine_test_run, safety_induction, operator_handbook, date, completed_by,
+                                 created)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+-- name: UpdateRegistration :exec
+update MachineRegistration
+set dealer_name       = ?,
+    dealer_address    = ?,
+    owner_name        = ?,
+    owner_address     = ?,
+    machine_model     = ?,
+    serial_number     = ?,
+    install_date      = ?,
+    invoice_number    = ?,
+    complete_supply   = ?,
+    pdi_complete      = ?,
+    pto_correct       = ?,
+    machine_test_run  = ?,
+    safety_induction  = ?,
+    operator_handbook = ?,
+    date              = ?,
+    completed_by      = ?
+where id = ?;
+
+-- name: DeleteRegistration :exec
+delete
+from MachineRegistration
+where id = ?;
