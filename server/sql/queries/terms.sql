f39707aea8affd98a72495f66cc56eacc2b863f8
seanwelch <seanwelch51199@icloud.com>
2024-08-29 20:22:40 +0100

    terms sql

---
+++ server/sql/queries/terms.sql
@@ -0,0 +1,25 @@
+-- name: GetTerms :many
+select id, title, body, created
+from Terms;
+
+-- name: GetTermByID :one
+select id, title, body, created
+from Terms
+where id = ?;
+
+-- name: CreateTerm :exec
+insert into Terms (id, title, body, created)
+values (?, ?, ?, ?);
+
+-- name: UpdateTerm :exec
+update Terms
+set id      = ?,
+    title   = ?,
+    body    = ?,
+    created = ?
+where id = ?;
+
+-- name: DeleteTerm :exec
+delete
+from Terms
+where id = ?;
