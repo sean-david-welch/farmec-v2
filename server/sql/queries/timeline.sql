c0b54a3207c4ef22d51207c6fd3725123555c484
seanwelch <seanwelch51199@icloud.com>
2024-08-29 20:33:12 +0100

    format

--- server/sql/queries/timeline.sql
+++ server/sql/queries/timeline.sql
@@ -1,14 +1,24 @@
 -- name: GetTimelines :many
-select id, title, date, body, created from Timeline;
+select id, title, date, body, created
+from Timeline;
 
 -- name: GetTimelineByID :one
-select id, title, date, body, created from Timeline where id = ?;
+select id, title, date, body, created
+from Timeline
+where id = ?;
 
 -- name: CreateTimeline :exec
-insert into Timeline (id, title, date, body, created) values (?, ?, ?, ?, ?);
+insert into Timeline (id, title, date, body, created)
+values (?, ?, ?, ?, ?);
 
 -- name: UpdateTimeline :exec
-update Timeline set title = ?, date = ?, body = ? where id = ?;
+update Timeline
+set title = ?,
+    date  = ?,
+    body  = ?
+where id = ?;
 
 -- name: DeleteTimeline :exec
-delete from Timeline where id = ?;
+delete
+from Timeline
+where id = ?;
