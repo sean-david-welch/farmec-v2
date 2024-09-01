334fb1920c70a1345cc858a1898ba0e4fbf961bb
seanwelch <seanwelch51199@icloud.com>
2024-09-01 17:30:44 +0100

    some terms

--- server/stores/termsStore.go
+++ server/stores/termsStore.go
@@ -1,66 +1,53 @@
 package stores
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/sean-david-welch/farmec-v2/server/types"
+	"github.com/sean-david-welch/farmec-v2/server/db"
 )
 
 type TermsStore interface {
-	GetTerms() ([]types.Terms, error)
-	CreateTerm(term *types.Terms) error
-	UpdateTerm(id string, term *types.Terms) error
-	DeleteTerm(id string) error
+	GetTerms(ctx context.Context) ([]db.Term, error)
+	CreateTerm(ctx context.Context, term *db.Term) error
+	UpdateTerm(ctx context.Context, id string, term *db.Term) error
+	DeleteTerm(ctx context.Context, id string) error
 }
 
 type TermsStoreImpl struct {
-	database *sql.DB
+	queries *db.Queries
 }
 
-func NewTermsStore(database *sql.DB) *TermsStoreImpl {
-	return &TermsStoreImpl{database: database}
+func NewTermsStore(sql *sql.DB) *TermsStoreImpl {
+	queries := db.New(sql)
+	return &TermsStoreImpl{queries: queries}
 }
 
-func (store *TermsStoreImpl) GetTerms() ([]types.Terms, error) {
-	var terms []types.Terms
-
-	query := `SELECT * FROM "Terms"`
-	rows, err := store.database.Query(query)
+func (store *TermsStoreImpl) GetTerms(ctx context.Context) ([]db.Term, error) {
+	terms, err := store.queries.GetTerms(ctx)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error occurred while querying the database for terms: %w", err)
 	}
-	defer func() {
-		if err := rows.Close(); err != nil {
-			log.Fatal("Failed to close database: ", err)
-		}
-	}()
-
-	for rows.Next() {
-		var term types.Terms
-
-		if err := rows.Scan(&term.ID, &term.Title, &term.Body, &term.Created); err != nil {
-			return nil, fmt.Errorf("error occurred while scanning row: %w", err)
-		}
-
-		terms = append(terms, term)
-
-		if err := rows.Err(); err != nil {
-			return nil, fmt.Errorf("error occurred after iterating over rows: %w", err)
-		}
+	var result []db.Term
+	for _, term := range terms {
+		result = append(result, db.Term{
+			ID:      term.ID,
+			Title:   term.Title,
+			Body:    term.Body,
+			Created: term.Created,
+		})
 	}
-
-	return terms, err
+	return result, nil
 }
 
-func (store *TermsStoreImpl) CreateTerm(term *types.Terms) error {
+func (store *TermsStoreImpl) CreateTerm(term *db.Term) error {
 	term.ID = uuid.NewString()
 	term.Created = time.Now().String()
 
-	query := `INSERT INTO "Terms" (id, title, body, created) VALUES (?, ?, ?, ?)`
+	query := `INSERT INTO "Term" (id, title, body, created) VALUES (?, ?, ?, ?)`
 
 	_, err := store.database.Exec(query, term.ID, term.Title, term.Body, term.Created)
 	if err != nil {
@@ -70,8 +57,8 @@ func (store *TermsStoreImpl) CreateTerm(term *types.Terms) error {
 	return nil
 }
 
-func (store *TermsStoreImpl) UpdateTerm(id string, term *types.Terms) error {
-	query := `UPDATE "Terms" SET title = ?, body = ? where id = ?`
+func (store *TermsStoreImpl) UpdateTerm(id string, term *db.Term) error {
+	query := `UPDATE "Term" SET title = ?, body = ? where id = ?`
 
 	_, err := store.database.Exec(query, term.Title, term.Body, id)
 	if err != nil {
@@ -82,7 +69,7 @@ func (store *TermsStoreImpl) UpdateTerm(id string, term *types.Terms) error {
 }
 
 func (store *TermsStoreImpl) DeleteTerm(id string) error {
-	query := `DELETE FROM "Terms" WHERE "id" = ?`
+	query := `DELETE FROM "Term" WHERE "id" = ?`
 
 	_, err := store.database.Exec(query, id)
 	if err != nil {
