67d2257a1fab42d6de1dc3892c6b165f9be488c7
seanwelch <seanwelch51199@icloud.com>
2024-09-01 16:59:09 +0100

    timeline create

--- server/stores/timelineStore.go
+++ server/stores/timelineStore.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/google/uuid"
@@ -19,54 +18,51 @@ type TimelineStore interface {
 }
 
 type TimelineStoreImpl struct {
-	database *sql.DB
+	queries *db.Queries
 }
 
-func NewTimelineStore(database *sql.DB) *TimelineStoreImpl {
-	return &TimelineStoreImpl{database: database}
+func NewTimelineStore(sql *sql.DB) *TimelineStoreImpl {
+	queries := db.New(sql)
+	return &TimelineStoreImpl{queries: queries}
 }
 
-func (store *TimelineStoreImpl) GetTimelines() ([]db.Timeline, error) {
-	var timelines []db.Timeline
-
-	query := `SELECT * FROM "Timeline"`
-	rows, err := store.database.Query(query)
+func (store *TimelineStoreImpl) GetTimelines(ctx context.Context) ([]db.Timeline, error) {
+	timelines, err := store.queries.GetTimelines(ctx)
 	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if err := rows.Close(); err != nil {
-			log.Fatal("Failed to close database: ", err)
-		}
-	}()
-
-	for rows.Next() {
-		var timeline db.Timeline
-
-		if err := rows.Scan(&timeline.ID, &timeline.Title, &timeline.Date, &timeline.Body, &timeline.Created); err != nil {
-			return nil, fmt.Errorf("error occurred while scanning rows: %w", err)
-		}
-		timelines = append(timelines, timeline)
+		return nil, fmt.Errorf("error occurred while getting timelines from the db: %w", err)
 	}
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error occurred after iterating over rows: %w", err)
+	var result []db.Timeline
+	for _, timeline := range timelines {
+		result = append(result, db.Timeline{
+			ID:      timeline.ID,
+			Title:   timeline.Title,
+			Date:    timeline.Date,
+			Body:    timeline.Body,
+			Created: timeline.Created,
+		})
 	}
-
-	return timelines, nil
+	return result, nil
 }
 
-func (store *TimelineStoreImpl) CreateTimeline(timeline *db.Timeline) error {
+func (store *TimelineStoreImpl) CreateTimeline(ctx context.Context, timeline *db.Timeline) error {
 	timeline.ID = uuid.NewString()
-	timeline.Created = time.Now().String()
-
-	query := `INSERT INTO "Timeline" (id, title, date, body, created) VALUES (?, ?, ?, ?, ?)`
-
-	_, err := store.database.Exec(query, timeline.ID, timeline.Title, timeline.Date, timeline.Body, timeline.Created)
-	if err != nil {
-		return fmt.Errorf("error occurred while creating timeline: %w", err)
+	timeline.Created = sql.NullString{
+		String: time.Now().String(),
+		Valid:  true,
 	}
 
+	params := db.CreateTimelineParams{
+		ID:      timeline.ID,
+		Title:   timeline.Title,
+		Date:    timeline.Date,
+		Body:    timeline.Body,
+		Created: timeline.Created,
+	}
+
+	if err := store.queries.CreateTimeline(ctx, params); err != nil {
+		return fmt.Errorf("error occurred while creating a timeline: %w", err)
+	}
 	return nil
 }
 
