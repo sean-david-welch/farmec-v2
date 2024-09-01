13158990f857470f0a1ab686336b1fa71d223ad9
seanwelch <seanwelch51199@icloud.com>
2024-09-01 16:45:25 +0100

    some video store

--- server/stores/videoStore.go
+++ server/stores/videoStore.go
@@ -1,78 +1,75 @@
 package stores
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/sean-david-welch/farmec-v2/server/types"
+	"github.com/sean-david-welch/farmec-v2/server/db"
 )
 
 type VideoStore interface {
-	GetVideos(id string) ([]types.Video, error)
-	CreateVideo(video *types.Video) error
-	UpdateVideo(id string, video *types.Video) error
-	DeleteVideo(id string) error
+	GetVideos(ctx context.Context, id string) ([]db.Video, error)
+	CreateVideo(ctx context.Context, video *db.Video) error
+	UpdateVideo(ctx context.Context, id string, video *db.Video) error
+	DeleteVideo(ctx context.Context, id string) error
 }
 
 type VideoStoreImpl struct {
-	database *sql.DB
+	queries *db.Queries
 }
 
-func NewVideoStore(database *sql.DB) *VideoStoreImpl {
-	return &VideoStoreImpl{database: database}
+func NewVideoStore(sql *sql.DB) *VideoStoreImpl {
+	queries := db.New(sql)
+	return &VideoStoreImpl{queries: queries}
 }
 
-func (store *VideoStoreImpl) GetVideos(id string) ([]types.Video, error) {
-	var videos []types.Video
-
-	query := `SELECT * FROM "Video" WHERE "supplier_id" = ?`
-	rows, err := store.database.Query(query, id)
+func (store *VideoStoreImpl) GetVideos(ctx context.Context, id string) ([]db.Video, error) {
+	videos, err := store.queries.SelectVideos(ctx, id)
 	if err != nil {
-		return nil, fmt.Errorf("error executing query: %w", err)
+		return nil, fmt.Errorf("error occurred while getting videos from the database: %w", err)
 	}
-	defer func() {
-		if err := rows.Close(); err != nil {
-			log.Fatal("Failed to close database: ", err)
-		}
-	}()
-
-	for rows.Next() {
-		var video types.Video
-
-		err := rows.Scan(&video.ID, &video.SupplierID, &video.WebURL, &video.Title, &video.Description, &video.VideoID, &video.ThumbnailURL, &video.Created)
-		if err != nil {
-			return nil, fmt.Errorf("error scanning row: %w", err)
-		}
-
-		videos = append(videos, video)
+	var result []db.Video
+	for _, video := range videos {
+		result = append(result, db.Video{
+			ID:           video.ID,
+			SupplierID:   video.SupplierID,
+			WebUrl:       video.WebUrl,
+			Title:        video.Title,
+			Description:  video.Description,
+			VideoID:      video.VideoID,
+			ThumbnailUrl: video.ThumbnailUrl,
+			Created:      video.Created,
+		})
 	}
-
-	if err = rows.Err(); err != nil {
-		return nil, fmt.Errorf("error after iterating over rows: %w", err)
-	}
-
-	return videos, nil
+	return result, nil
 }
 
-func (store *VideoStoreImpl) CreateVideo(video *types.Video) error {
-
+func (store *VideoStoreImpl) CreateVideo(ctx context.Context, video *db.Video) error {
 	video.ID = uuid.NewString()
-	video.Created = time.Now().String()
-
-	query := `INSERT INTO "Video" ("id", "supplier_id", "web_url", "title", "description", "video_id", "thumbnail_url", "created") VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
-	_, err := store.database.Exec(query, video.ID, video.SupplierID, video.WebURL, video.Title, video.Description, video.VideoID, video.ThumbnailURL, video.Created)
-
-	if err != nil {
-		return fmt.Errorf("error creating video: %w", err)
+	video.Created = sql.NullString{
+		String: time.Now().String(),
+		Valid:  true,
 	}
-
+	params := db.CreateVideoParams{
+		ID:           video.ID,
+		SupplierID:   video.SupplierID,
+		WebUrl:       video.WebUrl,
+		Title:        video.Title,
+		Description:  video.Description,
+		VideoID:      video.VideoID,
+		ThumbnailUrl: video.ThumbnailUrl,
+		Created:      video.Created,
+	}
+	if err := store.queries.CreateVideo(ctx, params); err != nil {
+		return fmt.Errorf("error occurred while creating a video in the db: %w", err)
+	}
 	return nil
 }
 
-func (store *VideoStoreImpl) UpdateVideo(id string, video *types.Video) error {
+func (store *VideoStoreImpl) UpdateVideo(id string, video *db.Video) error {
 	query := `UPDATE "Video" SET "supplier_id" = ?, "web_url" = ?, "title" = ?, "description" = ?, "video_id" = ?, "thumbnail_url" = ? WHERE "id" = ?`
 
 	_, err := store.database.Exec(query, video.SupplierID, video.WebURL, video.Title, video.Description, video.VideoID, video.ThumbnailURL, id)
