e890e98e2ff54ef962c5b1d2241dac8f03e21072
seanwelch <seanwelch51199@icloud.com>
2024-09-02 19:38:41 +0100

    registration store

--- server/stores/registrationStore.go
+++ server/stores/registrationStore.go
@@ -1,72 +1,66 @@
 package stores
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
+	"github.com/sean-david-welch/farmec-v2/server/db"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
 type RegistrationStore interface {
-	GetRegistrations() ([]types.MachineRegistration, error)
-	GetRegistrationById(id string) (*types.MachineRegistration, error)
-	CreateRegistration(registration *types.MachineRegistration) error
-	UpdateRegistration(id string, registration *types.MachineRegistration) error
-	DeleteRegistration(id string) error
+	GetRegistrations(ctx context.Context) ([]db.MachineRegistration, error)
+	GetRegistrationById(ctx context.Context, id string) (*db.MachineRegistration, error)
+	CreateRegistration(ctx context.Context, registration *db.MachineRegistration) error
+	UpdateRegistration(ctx context.Context, id string, registration *db.MachineRegistration) error
+	DeleteRegistration(ctx context.Context, id string) error
 }
 
 type RegistrationStoreImpl struct {
-	database *sql.DB
+	queries *db.Queries
 }
 
-func NewRegistrationStore(database *sql.DB) *RegistrationStoreImpl {
-	return &RegistrationStoreImpl{database: database}
+func NewRegistrationStore(sql *sql.DB) *RegistrationStoreImpl {
+	queries := db.New(sql)
+	return &RegistrationStoreImpl{queries: queries}
 }
 
-func (store *RegistrationStoreImpl) GetRegistrations() ([]types.MachineRegistration, error) {
-	var registrations []types.MachineRegistration
-
-	query := `SELECT * FROM "MachineRegistration" ORDER BY "created" DESC`
-	rows, err := store.database.Query(query)
+func (store *RegistrationStoreImpl) GetRegistrations(ctx context.Context) ([]db.MachineRegistration, error) {
+	registrations, err := store.queries.GetRegistrations(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("error while querying database: %w", err)
-	}
-	defer func() {
-		if err := rows.Close(); err != nil {
-			log.Fatal("Failed to close database: ", err)
-		}
-	}()
-
-	for rows.Next() {
-		var registration types.MachineRegistration
-
-		err := rows.Scan(
-			&registration.ID, &registration.DealerName, &registration.DealerAddress,
-			&registration.OwnerName, &registration.OwnerAddress, &registration.MachineModel,
-			&registration.SerialNumber, &registration.InstallDate, &registration.InvoiceNumber,
-			&registration.CompleteSupply, &registration.PdiComplete, &registration.PtoCorrect,
-			&registration.MachineTestRun, &registration.SafetyInduction, &registration.OperatorHandbook,
-			&registration.Date, &registration.CompletedBy, &registration.Created,
-		)
-		if err != nil {
-			return nil, fmt.Errorf("error occurred while iterating over rows: %w", err)
-		}
-
-		registrations = append(registrations, registration)
+		return nil, fmt.Errorf("error occurred while getting registration from the db: %w", err)
 	}
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error occurred after iterating over rows: %w", err)
+	var result []db.MachineRegistration
+	for _, registration := range registrations {
+		result = append(result, db.MachineRegistration{
+			ID:               registration.ID,
+			DealerName:       registration.DealerName,
+			DealerAddress:    registration.DealerAddress,
+			OwnerName:        registration.OwnerName,
+			OwnerAddress:     registration.OwnerAddress,
+			MachineModel:     registration.MachineModel,
+			SerialNumber:     registration.SerialNumber,
+			InstallDate:      registration.InstallDate,
+			InvoiceNumber:    registration.InvoiceNumber,
+			CompleteSupply:   registration.CompleteSupply,
+			PdiComplete:      registration.PdiComplete,
+			PtoCorrect:       registration.PtoCorrect,
+			MachineTestRun:   registration.MachineTestRun,
+			SafetyInduction:  registration.SafetyInduction,
+			OperatorHandbook: registration.OperatorHandbook,
+			Date:             registration.Date,
+			CompletedBy:      registration.CompletedBy,
+			Created:          registration.Created,
+		})
 	}
-
-	return registrations, nil
+	return result, nil
 }
 
-func (store *RegistrationStoreImpl) GetRegistrationById(id string) (*types.MachineRegistration, error) {
-	var registration types.MachineRegistration
+func (store *RegistrationStoreImpl) GetRegistrationById(ctx context.Context, id string) (*db.MachineRegistration, error) {
+	var registration db.MachineRegistration
 
 	query := `SELECT * FROM "MachineRegistration" WHERE "id" = ?`
 	row := store.database.QueryRow(query, id)
@@ -85,7 +79,7 @@ func (store *RegistrationStoreImpl) GetRegistrationById(id string) (*types.Machi
 	return &registration, nil
 }
 
-func (store *RegistrationStoreImpl) CreateRegistration(registration *types.MachineRegistration) error {
+func (store *RegistrationStoreImpl) CreateRegistration(ctx context.Context, registration *db.MachineRegistration) error {
 	registration.ID = uuid.NewString()
 	registration.Created = time.Now().String()
 
@@ -112,7 +106,7 @@ func (store *RegistrationStoreImpl) CreateRegistration(registration *types.Machi
 	return nil
 }
 
-func (store *RegistrationStoreImpl) UpdateRegistration(id string, registration *types.MachineRegistration) error {
+func (store *RegistrationStoreImpl) UpdateRegistration(ctx context.Context, id string, registration *db.MachineRegistration) error {
 	query := `UPDATE "MachineRegistration" SET 
 	"dealer_name" = ?, "dealer_address" = ?, "owner_name" = ?, "owner_address" = ?, 
 	"machine_model" = ?, "serial_number" = ?, "install_date" = ?, "invoice_number" = ?, 
@@ -136,7 +130,7 @@ func (store *RegistrationStoreImpl) UpdateRegistration(id string, registration *
 	return nil
 }
 
-func (store *RegistrationStoreImpl) DeleteRegistration(id string) error {
+func (store *RegistrationStoreImpl) DeleteRegistration(ctx context.Context, id string) error {
 	query := `DELETE FROM "MachineRegistration" WHERE "id" = ?`
 
 	_, err := store.database.Exec(query, id)
