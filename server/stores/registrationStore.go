bcd981c0e2c28d448eb3435ca9226a1de4d77936
seanwelch <seanwelch51199@icloud.com>
2024-09-02 19:50:22 +0100

    registration create

--- server/stores/registrationStore.go
+++ server/stores/registrationStore.go
@@ -60,49 +60,43 @@ func (store *RegistrationStoreImpl) GetRegistrations(ctx context.Context) ([]db.
 }
 
 func (store *RegistrationStoreImpl) GetRegistrationById(ctx context.Context, id string) (*db.MachineRegistration, error) {
-	var registration db.MachineRegistration
-
-	query := `SELECT * FROM "MachineRegistration" WHERE "id" = ?`
-	row := store.database.QueryRow(query, id)
-
-	if err := row.Scan(
-		&registration.ID, &registration.DealerName, &registration.DealerAddress,
-		&registration.OwnerName, &registration.OwnerAddress, &registration.MachineModel,
-		&registration.SerialNumber, &registration.InstallDate, &registration.InvoiceNumber,
-		&registration.CompleteSupply, &registration.PdiComplete, &registration.PtoCorrect,
-		&registration.MachineTestRun, &registration.SafetyInduction, &registration.OperatorHandbook,
-		&registration.Date, &registration.CompletedBy, &registration.Created,
-	); err != nil {
-		return nil, fmt.Errorf("error occurred while iterating over row: %w", err)
+	registration, err := store.queries.GetRegistrationsByID(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while getting a registration: %w", err)
 	}
-
 	return &registration, nil
 }
 
 func (store *RegistrationStoreImpl) CreateRegistration(ctx context.Context, registration *db.MachineRegistration) error {
 	registration.ID = uuid.NewString()
-	registration.Created = time.Now().String()
-
-	query := `INSERT INTO "MachineRegistration" (
-        "id", "dealer_name", "dealer_address", "owner_name", "owner_address", "machine_model", 
-        "serial_number", "install_date", "invoice_number", "complete_supply", "pdi_complete", 
-        "pto_correct", "machine_test_run", "safety_induction", "operator_handbook", "date", 
-        "completed_by", "created"
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
-
-	_, err := store.database.Exec(
-		query,
-		registration.ID, registration.DealerName, registration.DealerAddress, registration.OwnerName,
-		registration.OwnerAddress, registration.MachineModel, registration.SerialNumber,
-		registration.InstallDate, registration.InvoiceNumber, registration.CompleteSupply,
-		registration.PdiComplete, registration.PtoCorrect, registration.MachineTestRun,
-		registration.SafetyInduction, registration.OperatorHandbook, registration.Date,
-		registration.CompletedBy, registration.Created,
-	)
-	if err != nil {
-		return fmt.Errorf("error occurred while creating machine registration: %w", err)
+	registration.Created = sql.NullString{
+		String: time.Now().String(),
+		Valid:  true,
 	}
 
+	params := db.CreateRegistrationParams{
+		ID:               registration.ID,
+		DealerName:       registration.DealerName,
+		DealerAddress:    registration.DealerAddress,
+		OwnerName:        registration.OwnerName,
+		OwnerAddress:     registration.OwnerAddress,
+		MachineModel:     registration.MachineModel,
+		SerialNumber:     registration.SerialNumber,
+		InstallDate:      registration.InstallDate,
+		InvoiceNumber:    registration.InvoiceNumber,
+		CompleteSupply:   registration.CompleteSupply,
+		PdiComplete:      registration.PdiComplete,
+		PtoCorrect:       registration.PtoCorrect,
+		MachineTestRun:   registration.MachineTestRun,
+		SafetyInduction:  registration.SafetyInduction,
+		OperatorHandbook: registration.OperatorHandbook,
+		Date:             registration.Date,
+		CompletedBy:      registration.CompletedBy,
+		Created:          registration.Created,
+	}
+	if err := store.queries.CreateRegistration(ctx, params); err != nil {
+		return fmt.Errorf("error occurred while creating a registration: %w", err)
+	}
 	return nil
 }
 
