149ae30a1bd4e710de341e3f48a5c7696233eec1
seanwelch <seanwelch51199@icloud.com>
2024-09-05 12:52:49 +0100

    terms handler

--- server/handlers/termsHandler.go
+++ server/handlers/termsHandler.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sean-david-welch/farmec-v2/server/db"
 	"github.com/sean-david-welch/farmec-v2/server/services"
-	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
 type TermsHandler struct {
@@ -18,7 +18,8 @@ func NewTermsHandler(service services.TermsService) *TermsHandler {
 }
 
 func (handler *TermsHandler) GetTerms(context *gin.Context) {
-	terms, err := handler.service.GetTerms()
+	ctx := context.Request.Context()
+	terms, err := handler.service.GetTerms(ctx)
 	if err != nil {
 		log.Printf("error getting terms: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while getting term"})
@@ -29,14 +30,15 @@ func (handler *TermsHandler) GetTerms(context *gin.Context) {
 }
 
 func (handler *TermsHandler) CreateTerm(context *gin.Context) {
-	var term types.Terms
+	ctx := context.Request.Context()
+	var term db.Term
 
 	if err := context.ShouldBindJSON(&term); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
 		return
 	}
 
-	if err := handler.service.CreateTerm(&term); err != nil {
+	if err := handler.service.CreateTerm(ctx, &term); err != nil {
 		log.Printf("error while creating term: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating term", "details": err.Error()})
 		return
@@ -46,15 +48,16 @@ func (handler *TermsHandler) CreateTerm(context *gin.Context) {
 }
 
 func (handler *TermsHandler) UpdateTerm(context *gin.Context) {
+	ctx := context.Request.Context()
 	id := context.Param("id")
-	var term types.Terms
+	var term db.Term
 
 	if err := context.ShouldBindJSON(&term); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
 		return
 	}
 
-	if err := handler.service.UpdateTerm(id, &term); err != nil {
+	if err := handler.service.UpdateTerm(ctx, id, &term); err != nil {
 		log.Printf("error while updating term: %v", err)
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Error occurred while updating term", "details": err.Error()})
 		return
@@ -64,9 +67,10 @@ func (handler *TermsHandler) UpdateTerm(context *gin.Context) {
 }
 
 func (handler *TermsHandler) DeleteTerm(context *gin.Context) {
+	ctx := context.Request.Context()
 	id := context.Param("id")
 
-	if err := handler.service.DeleteTerm(id); err != nil {
+	if err := handler.service.DeleteTerm(ctx, id); err != nil {
 		log.Printf("Error deleting term: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while deleting term", "details": err.Error()})
 		return
