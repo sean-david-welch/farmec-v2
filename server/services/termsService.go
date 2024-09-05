95cf42255f46cd6a33bca06f121ebe6f504b711b
seanwelch <seanwelch51199@icloud.com>
2024-09-05 12:50:55 +0100

    terms service

--- server/services/termsService.go
+++ server/services/termsService.go
@@ -1,15 +1,16 @@
 package services
 
 import (
+	"context"
+	"github.com/sean-david-welch/farmec-v2/server/db"
 	"github.com/sean-david-welch/farmec-v2/server/stores"
-	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
 type TermsService interface {
-	GetTerms() ([]types.Terms, error)
-	CreateTerm(term *types.Terms) error
-	UpdateTerm(id string, term *types.Terms) error
-	DeleteTerm(id string) error
+	GetTerms(ctx context.Context) ([]db.Term, error)
+	CreateTerm(ctx context.Context, term *db.Term) error
+	UpdateTerm(ctx context.Context, id string, term *db.Term) error
+	DeleteTerm(ctx context.Context, id string) error
 }
 
 type TermsServiceImpl struct {
@@ -20,35 +21,31 @@ func NewTermsService(store stores.TermsStore) *TermsServiceImpl {
 	return &TermsServiceImpl{store: store}
 }
 
-func (service *TermsServiceImpl) GetTerms() ([]types.Terms, error) {
-	terms, err := service.store.GetTerms()
+func (service *TermsServiceImpl) GetTerms(ctx context.Context) ([]db.Term, error) {
+	terms, err := service.store.GetTerms(ctx)
 	if err != nil {
 		return nil, err
 	}
-
 	return terms, nil
 }
 
-func (service *TermsServiceImpl) CreateTerm(term *types.Terms) error {
-	if err := service.store.CreateTerm(term); err != nil {
+func (service *TermsServiceImpl) CreateTerm(ctx context.Context, term *db.Term) error {
+	if err := service.store.CreateTerm(ctx, term); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (service *TermsServiceImpl) UpdateTerm(id string, term *types.Terms) error {
-	if err := service.store.UpdateTerm(id, term); err != nil {
+func (service *TermsServiceImpl) UpdateTerm(ctx context.Context, id string, term *db.Term) error {
+	if err := service.store.UpdateTerm(ctx, id, term); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (service *TermsServiceImpl) DeleteTerm(id string) error {
-	if err := service.store.DeleteTerm(id); err != nil {
+func (service *TermsServiceImpl) DeleteTerm(ctx context.Context, id string) error {
+	if err := service.store.DeleteTerm(ctx, id); err != nil {
 		return err
 	}
-
 	return nil
 }
