a2d5b174472e6466655c37f4245e3cc792116381
Sean Welch <seanwelch51199@icloud.com>
2024-10-13 20:45:16 +0100

    termsHandler.go

--- server/handlers/termsHandler.go
+++ server/handlers/termsHandler.go
@@ -1,12 +1,13 @@
 package handlers
 
 import (
+	"github.com/sean-david-welch/farmec-v2/server/lib"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sean-david-welch/farmec-v2/server/db"
 	"github.com/sean-david-welch/farmec-v2/server/services"
+	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
 type TermsHandler struct {
@@ -31,14 +32,15 @@ func (handler *TermsHandler) GetTerms(context *gin.Context) {
 
 func (handler *TermsHandler) CreateTerm(context *gin.Context) {
 	ctx := context.Request.Context()
-	var term db.Term
+	var term types.Terms
+	dbTerm := lib.DeserializeTerm(term)
 
 	if err := context.ShouldBindJSON(&term); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
 		return
 	}
 
-	if err := handler.service.CreateTerm(ctx, &term); err != nil {
+	if err := handler.service.CreateTerm(ctx, &dbTerm); err != nil {
 		log.Printf("error while creating term: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating term", "details": err.Error()})
 		return
@@ -50,14 +52,15 @@ func (handler *TermsHandler) CreateTerm(context *gin.Context) {
 func (handler *TermsHandler) UpdateTerm(context *gin.Context) {
 	ctx := context.Request.Context()
 	id := context.Param("id")
-	var term db.Term
+	var term types.Terms
+	dbTerm := lib.DeserializeTerm(term)
 
 	if err := context.ShouldBindJSON(&term); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body", "details": err.Error()})
 		return
 	}
 
-	if err := handler.service.UpdateTerm(ctx, id, &term); err != nil {
+	if err := handler.service.UpdateTerm(ctx, id, &dbTerm); err != nil {
 		log.Printf("error while updating term: %v", err)
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Error occurred while updating term", "details": err.Error()})
 		return
