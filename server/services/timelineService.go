2c191d4b34a2cb2e13fbbfa9161cabce6b92411e
Sean Welch <seanwelch51199@icloud.com>
2024-10-13 20:48:07 +0100

    timelineService.go

--- server/services/timelineService.go
+++ server/services/timelineService.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 	"github.com/sean-david-welch/farmec-v2/server/db"
+	"github.com/sean-david-welch/farmec-v2/server/lib"
 	"github.com/sean-david-welch/farmec-v2/server/stores"
+	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
 type TimelineService interface {
-	GetTimelines(ctx context.Context) ([]db.Timeline, error)
+	GetTimelines(ctx context.Context) ([]types.Timeline, error)
 	CreateTimeline(ctx context.Context, timeline *db.Timeline) error
 	UpdateTimeline(ctx context.Context, id string, timeline *db.Timeline) error
 	DeleteTimeline(ctx context.Context, id string) error
@@ -21,12 +23,16 @@ func NewTimelineService(store stores.TimelineStore) *TimelineServiceImpl {
 	return &TimelineServiceImpl{store: store}
 }
 
-func (service *TimelineServiceImpl) GetTimelines(ctx context.Context) ([]db.Timeline, error) {
+func (service *TimelineServiceImpl) GetTimelines(ctx context.Context) ([]types.Timeline, error) {
 	timelines, err := service.store.GetTimelines(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return timelines, nil
+	var result []types.Timeline
+	for _, event := range timelines {
+		result = append(result, lib.SerializeTimeline(event))
+	}
+	return result, nil
 }
 
 func (service *TimelineServiceImpl) CreateTimeline(ctx context.Context, timeline *db.Timeline) error {
