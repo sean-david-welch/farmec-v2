bdece8312b077355023165ec9f72c3a26b8edf97
Sean Welch <seanwelch51199@icloud.com>
2024-10-19 14:03:27 +0100

    videoHandler.go

--- server/handlers/videoHandler.go
+++ server/handlers/videoHandler.go
@@ -35,13 +35,13 @@ func (handler *VideoHandler) GetVideos(context *gin.Context) {
 func (handler *VideoHandler) CreateVideo(context *gin.Context) {
 	ctx := context.Request.Context()
 	var video types.Video
-	dbVideo := lib.DeserializeVideo(video)
 
 	if err := context.ShouldBindJSON(&video); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request Body", "Details": err.Error()})
 		return
 	}
 
+	dbVideo := lib.DeserializeVideo(video)
 	if err := handler.service.CreateVideo(ctx, &dbVideo); err != nil {
 		log.Printf("Error creating video: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating video", "details": err.Error()})
@@ -56,13 +56,12 @@ func (handler *VideoHandler) UpdateVideo(context *gin.Context) {
 	id := context.Param("id")
 
 	var video types.Video
-	dbVideo := lib.DeserializeVideo(video)
-
 	if err := context.ShouldBindJSON(&video); err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Request Body", "Details": err.Error()})
 		return
 	}
 
+	dbVideo := lib.DeserializeVideo(video)
 	if err := handler.service.UpdateVideo(ctx, id, &dbVideo); err != nil {
 		log.Printf("Error updating video: %v", err)
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while updating video", "details": err.Error()})
