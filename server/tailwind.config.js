f2ae6bc93b30e348f69a96196a64727dc6cc7132
Sean Welch <seanwelch51199@icloud.com>
2024-10-20 18:56:40 +0100

    tailwind and postcss

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./views/**/*.templ}", "./**/*.templ"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
