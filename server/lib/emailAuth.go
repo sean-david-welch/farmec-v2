7e033d85a4a1dedc189c322348588a5cca7f545d
Sean Welch <seanwelch51199@icloud.com>
2024-10-28 20:21:05 +0000

    add some changes

--- server/lib/emailAuth.go
+++ server/lib/emailAuth.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"errors"
 	"net/smtp"
 )
 
@@ -10,31 +9,15 @@ type EmailAuth interface {
 	Next(fromServer []byte, more bool) ([]byte, error)
 }
 
-type EmailAuthImpl struct {
-	username, password string
+type EmailAuthImpl interface {
+	smtp.Auth
 }
 
 func NewLoginAuth(username, password string) EmailAuth {
-	return &EmailAuthImpl{
-		username: username,
-		password: password,
-	}
-}
-
-func (auth *EmailAuthImpl) Start(_ *smtp.ServerInfo) (string, []byte, error) {
-	return "LOGIN", []byte{}, nil
-}
-
-func (auth *EmailAuthImpl) Next(fromServer []byte, more bool) ([]byte, error) {
-	if more {
-		switch string(fromServer) {
-		case "Username:":
-			return []byte(auth.username), nil
-		case "Password:":
-			return []byte(auth.password), nil
-		default:
-			return nil, errors.New("unknown from server")
-		}
-	}
-	return nil, nil
+	return smtp.PlainAuth(
+		"",
+		username,
+		password,
+		"smtp.office365.com",
+	)
 }
--- server/lib/smtp.go
+++ server/lib/smtp.go
@@ -4,7 +4,6 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/sean-david-welch/farmec-v2/server/types"
-	"io"
 	"net"
 	"net/smtp"
 	"time"
@@ -20,21 +19,21 @@ type SMTPClientImpl struct {
 	emailAuth EmailAuth
 }
 
-func NewSTMPClient(secrets *Secrets, emailAuth EmailAuth) *SMTPClientImpl {
-	return &SMTPClientImpl{secrets: secrets, emailAuth: emailAuth}
+func NewSTMPClient(secrets *Secrets) *SMTPClientImpl {
+	auth := NewLoginAuth(
+		secrets.EmailUser, // Should be full email address
+		secrets.EmailPass, // Should be app password
+	)
+	return &SMTPClientImpl{
+		secrets:   secrets,
+		emailAuth: auth,
+	}
 }
 
 func (service *SMTPClientImpl) SetupSMTPClient() (*smtp.Client, error) {
 	conn, err := net.DialTimeout("tcp", "smtp.office365.com:587", 10*time.Second)
 	if err != nil {
 		return nil, fmt.Errorf("failed to establish TCP connection: %w", err)
-	}
-
-	deadline := time.Now().Add(30 * time.Second)
-	err = conn.SetDeadline(deadline)
-	if err != nil {
-		err := conn.Close()
-		return nil, fmt.Errorf("failed to set deadline: %w", err)
 	}
 
 	client, err := smtp.NewClient(conn, "smtp.office365.com")
@@ -44,6 +43,14 @@ func (service *SMTPClientImpl) SetupSMTPClient() (*smtp.Client, error) {
 			return nil, fmt.Errorf("failed to create SMTP client: %w", err)
 		}
 		return nil, fmt.Errorf("failed to create SMTP client: %w", err)
+	}
+
+	if err = client.Hello("localhost"); err != nil {
+		err := client.Close()
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("HELO failed: %w", err)
 	}
 
 	tlsConfig := &tls.Config{ServerName: "smtp.office365.com", MinVersion: tls.VersionTLS12}
@@ -67,29 +74,47 @@ func (service *SMTPClientImpl) SetupSMTPClient() (*smtp.Client, error) {
 	return client, nil
 }
 
-func (service *SMTPClientImpl) SendFormNotification(client *smtp.Client, data *types.EmailData, form string) error {
-	msg := fmt.Sprintf("Subject: New %s Form from %s--%s\r\n\r\n%s", form, data.Name, data.Email, data.Message)
+func (service *SMTPClientImpl) SendFormNotification(data *types.EmailData, form string) error {
+	// Create new client for each send operation
+	client, err := service.SetupSMTPClient()
+	if err != nil {
+		return fmt.Errorf("failed to setup client: %w", err)
+	}
+	defer func(client *smtp.Client) {
+		err := client.Close()
+		if err != nil {
+			return
+		}
+	}(client)
+
+	msg := fmt.Sprintf("Subject: New %s Form from %s--%s\r\n\r\n%s",
+		form, data.Name, data.Email, data.Message)
 
 	if err := client.Mail(service.secrets.EmailUser); err != nil {
-		return err
+		return fmt.Errorf("MAIL FROM failed: %w", err)
 	}
+
 	if err := client.Rcpt(service.secrets.EmailUser); err != nil {
-		return err
+		return fmt.Errorf("RCPT TO failed: %w", err)
 	}
 
 	wc, err := client.Data()
 	if err != nil {
-		return err
+		return fmt.Errorf("DATA command failed: %w", err)
 	}
-	defer func(wc io.WriteCloser) {
+
+	_, err = wc.Write([]byte(msg))
+	if err != nil {
 		err := wc.Close()
 		if err != nil {
-			return
+			return err
 		}
-	}(wc)
+		return fmt.Errorf("write failed: %w", err)
+	}
 
-	if _, err := wc.Write([]byte(msg)); err != nil {
-		return err
+	err = wc.Close()
+	if err != nil {
+		return fmt.Errorf("close failed: %w", err)
 	}
 
 	return nil
