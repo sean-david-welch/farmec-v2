d1d482da92f2457447595024d532b580314af026
Sean Welch <seanwelch51199@icloud.com>
2024-11-09 11:38:35 +0000

    new email auth

--- server/lib/emailAuth.go
+++ server/lib/emailAuth.go
@@ -4,20 +4,27 @@ import (
 	"net/smtp"
 )
 
-type EmailAuth interface {
-	Start(_ *smtp.ServerInfo) (string, []byte, error)
-	Next(fromServer []byte, more bool) ([]byte, error)
+type LoginAuth struct {
+	username string
+	password string
+	host     string
 }
 
-type EmailAuthImpl interface {
-	smtp.Auth
+func (a *LoginAuth) Start(_ *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", []byte(a.username), nil
 }
 
-func NewLoginAuth(username, password string) EmailAuth {
-	return smtp.PlainAuth(
-		"",
-		username,
-		password,
-		"smtp.office365.com",
-	)
+func (a *LoginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		return []byte(a.password), nil
+	}
+	return nil, nil
+}
+
+func NewLoginAuth(username, password string) smtp.Auth {
+	return &LoginAuth{
+		username: username,
+		password: password,
+		host:     "smtp.office365.com",
+	}
 }
