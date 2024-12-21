cab13b282cd7a8a675d8a517d8d423132f8992a1
Sean Welch <seanwelch51199@icloud.com>
2024-12-21 16:05:18 +0000

    yeag

--- server/lib/smtp.go
+++ server/lib/smtp.go
@@ -24,7 +24,7 @@ func (service *SMTPClientImpl) SendEmail(to []string, subject, body string) erro
 	from := mail.Address{Name: "Farmec", Address: service.secrets.EmailUser}
 	message := strings.Builder{}
 	message.WriteString(fmt.Sprintf("From: %s\r\n", from.String()))
-	message.WriteString(fmt.Sprintf("To: %s\r\n", strings.Join(to, ", ")))
+	message.WriteString(fmt.Sprintf("To: %s\r\n", strings.Join(to, ", "))) // Changed separator to comma+space
 	message.WriteString(fmt.Sprintf("Subject: %s\r\n", subject))
 	message.WriteString("MIME-Version: 1.0\r\n")
 	message.WriteString("Content-Type: text/plain; charset=UTF-8\r\n")
