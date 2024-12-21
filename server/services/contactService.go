d5de386ced773fdd63214044110e2b481812ecc2
Sean Welch <seanwelch51199@icloud.com>
2024-12-21 15:53:54 +0000

    contactService.go

--- server/services/contactService.go
+++ server/services/contactService.go
@@ -2,8 +2,6 @@ package services
 
 import (
 	"github.com/sean-david-welch/farmec-v2/server/lib"
-	"net/smtp"
-
 	"github.com/sean-david-welch/farmec-v2/server/types"
 )
 
@@ -22,18 +20,7 @@ func NewContactService(smtpClient lib.SMTPClient) *ContactServiceImpl {
 }
 
 func (service *ContactServiceImpl) SendContactEmail(data *types.EmailData) error {
-	client, err := service.smtpClient.SetupSMTPClient()
-	if err != nil {
-		return err
-	}
-	defer func(client *smtp.Client) {
-		err := client.Close()
-		if err != nil {
-			return
-		}
-	}(client)
-
-	if err := service.smtpClient.SendFormNotification(client, data, "Contact"); err != nil {
+	if err := service.smtpClient.SendFormNotification(data, "Contact"); err != nil {
 		return err
 	}
 	return nil
