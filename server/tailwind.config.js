eeab0c1d3f071070a2e6241a967556d578de0750
Sean Welch <seanwelch51199@icloud.com>
2025-01-19 20:01:05 +0000

    tailwind

--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.templ}", "./**/*.templ"],
-  theme: {
-    extend: {},
-  },
+      theme: {
+          extend: {
+              screens: {
+                  md: { max: '896px' },
+                  lg: { min: '896px' },
+              },
+          },
+          plugins: [],
+      },
   plugins: [],
 }
