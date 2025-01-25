f6073393f72f685392bb72d92c49a16176d128f0
Sean Welch <seanwelch51199@icloud.com>
2025-01-25 17:00:03 +0000

    remove init suppliers

--- server/routes/supplierRoutes.go
+++ server/routes/supplierRoutes.go
@@ -1,25 +1,11 @@
 package routes
 
 import (
-	"database/sql"
-	"github.com/sean-david-welch/farmec-v2/server/lib"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/sean-david-welch/farmec-v2/server/handlers"
 	"github.com/sean-david-welch/farmec-v2/server/middleware"
-	"github.com/sean-david-welch/farmec-v2/server/repository"
-	"github.com/sean-david-welch/farmec-v2/server/services"
 )
-
-func InitSuppliers(router *gin.Engine, database *sql.DB, s3Client lib.S3Client, adminMiddleware *middleware.AdminMiddleware) services.SupplierService {
-	repo := repository.NewSupplierRepo(database)
-	service := services.NewSupplierService(repo, s3Client, "Suppliers")
-	handler := handlers.NewSupplierContoller(service)
-
-	SupplierRoutes(router, handler, adminMiddleware)
-	return service
-}
 
 func SupplierRoutes(router *gin.Engine, handler *handlers.SupplierHandler, adminMiddleware *middleware.AdminMiddleware) {
 	supplierGroup := router.Group("/api/suppliers")
