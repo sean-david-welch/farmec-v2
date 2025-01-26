048c3d97e85e126ce68cb55192ce3be4998593ab
Sean Welch <seanwelch51199@icloud.com>
2025-01-26 15:28:44 +0000

    fixed footer errs

--- server/views/footer_templ.go
+++ server/views/footer_templ.go
@@ -126,7 +126,7 @@ func toTopButton() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = Icon("arrow-up.svg", 64).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = Icon("arrow-up.svg", 8).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
