186540b1b990e004670a465c11ee89b3389760ce
Sean Welch <seanwelch51199@icloud.com>
2025-02-08 19:44:03 +0000

    move types

--- server/types/infoTypes.go
+++ server/types/infoTypes.go
@@ -18,3 +18,20 @@ type Exhibition struct {
 	Info     string `json:"info"`
 	Created  string `json:"created"`
 }
+
+type Employee struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	Role         string `json:"role"`
+	ProfileImage string `json:"profile_image"`
+	Created      string `json:"created"`
+}
+
+type Timeline struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Date    string `json:"date"`
+	Body    string `json:"body"`
+	Created string `json:"created"`
+}
