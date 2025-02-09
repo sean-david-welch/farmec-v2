eb6e075d0e85a062b5fd5c67a7f1ea38077e6add
Sean Welch <seanwelch51199@icloud.com>
2025-02-09 11:33:43 +0000

    remove prints

--- server/lib/emailClient.go
+++ server/lib/emailClient.go
@@ -18,7 +18,6 @@ type EmailClientImpl struct {
 }
 
 func NewEmailClient(secrets *Secrets) *EmailClientImpl {
-	log.Printf("Initializing email client with API key: %s", secrets.SendGridAPIKey)
 	client := sendgrid.NewSendClient(secrets.SendGridAPIKey)
 	return &EmailClientImpl{
 		secrets: secrets,
@@ -27,13 +26,6 @@ func NewEmailClient(secrets *Secrets) *EmailClientImpl {
 }
 
 func (service *EmailClientImpl) SendFormNotification(data *types.EmailData, form string) error {
-	log.Println("=== START EMAIL SENDING ===")
-	log.Printf("Form type: %s", form)
-	log.Printf("From email: %s", service.secrets.EmailUser)
-	log.Printf("To email: %s", service.secrets.EmailUser)
-	log.Printf("Sender name: %s", data.Name)
-	log.Printf("Sender email: %s", data.Email)
-
 	from := mail.NewEmail("Farmec Ireland Ltd", service.secrets.EmailUser)
 	to := mail.NewEmail("Admin", service.secrets.EmailUser)
 	subject := fmt.Sprintf("New %s Form from %s--%s", form, data.Name, data.Email)
@@ -55,7 +47,5 @@ func (service *EmailClientImpl) SendFormNotification(data *types.EmailData, form
 	if response.Body != "" {
 		log.Printf("Response body: %s", response.Body)
 	}
-	log.Println("=== END EMAIL SENDING ===")
-
 	return nil
 }
