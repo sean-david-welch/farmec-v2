a556e6f80d91753486b6afd511dde3138600d516
Sean Welch <seanwelch51199@icloud.com>
2025-02-16 15:00:37 +0000

    add

--- server/handlers/viewHandler.go
+++ server/handlers/viewHandler.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/sean-david-welch/farmec-v2/server/middleware"
 	"github.com/sean-david-welch/farmec-v2/server/services"
+	"github.com/sean-david-welch/farmec-v2/server/views/pages"
+	"log"
+	"net/http"
 )
 
 type ViewHandler struct {
@@ -13,4 +17,28 @@ type ViewHandler struct {
 
 func NewViewHandler(carouselService services.CarouselService, authMiddleware *middleware.AuthMiddlewareImpl, supplierCahce *middleware.SupplierCache) *ViewHandler {
 	return &ViewHandler{carouselService, authMiddleware, supplierCahce}
+}
+
+func (handler *ViewHandler) HomeView(c *gin.Context) {
+
+}
+
+func (handler *CarouselHandler) CarouselAdminView(context *gin.Context) {
+	request := context.Request.Context()
+	isAdmin := handler.authMiddleware.GetIsAdmin(context)
+	suppliers := handler.supplierCache.GetSuppliersFromContext(context)
+
+	carousels, err := handler.service.GetCarousels(request)
+	if err != nil {
+		log.Printf("Error getting carousels: %v\n", err)
+	}
+
+	isError := err != nil
+	component := pages.CarouselAdmin(isAdmin, isError, carousels, suppliers)
+	if err := component.Render(request, context.Writer); err != nil {
+		log.Printf("Error rendering carousels: %v\n", err)
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while rendering the page"})
+		return
+	}
+	context.Header("Content-Type", "text/html; charset=utf-8")
 }
