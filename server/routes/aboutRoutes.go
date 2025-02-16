9cb4a8649fe685f2c07ec419c1207b326e942059
Sean Welch <seanwelch51199@icloud.com>
2025-02-16 14:01:46 +0000

    fix routes

--- server/routes/aboutRoutes.go
+++ server/routes/aboutRoutes.go
@@ -15,32 +15,30 @@ func InitAbout(router *gin.Engine, database *sql.DB, s3Client lib.S3Client, auth
 	// Initialize Employee resources
 	employeeRepo := repository.NewEmployeeRepo(database)
 	employeeService := services.NewEmployeeService(employeeRepo, s3Client, "Employees")
-	employeeHandler := handlers.NewEmployeeHandler(employeeService)
 	// Initialize Timeline resources
 	timelineRepo := repository.NewTimelineRepo(database)
 	timelineService := services.NewTimelineService(timelineRepo)
 
 	// init routes
 	aboutHandler := handlers.NewAboutHandler(employeeService, timelineService, authMiddleware, supplierCache)
-	AboutRoutes(router, aboutHandler, employeeHandler, authMiddleware)
+	AboutRoutes(router, aboutHandler, authMiddleware)
 }
 
-func AboutRoutes(router *gin.Engine, aboutHandler *handlers.AboutHandler, employeeHandler *handlers.EmployeeHandler, authMiddleware *middleware.AuthMiddlewareImpl) {
+func AboutRoutes(router *gin.Engine, aboutHandler *handlers.AboutHandler, authMiddleware *middleware.AuthMiddlewareImpl) {
 	router.GET("/about", authMiddleware.ViewMiddleware(), aboutHandler.AboutView)
 	// Employee routes
 	employeeGroup := router.Group("/api/employees")
-	employeeGroup.GET("", employeeHandler.GetEmployees)
+	employeeGroup.GET("", aboutHandler.GetEmployees)
 	protectedEmployeeRoutes := employeeGroup.Group("").Use(authMiddleware.AdminRouteMiddleware())
 	{
-		protectedEmployeeRoutes.POST("", employeeHandler.CreateEmployee)
-		protectedEmployeeRoutes.PUT("/:id", employeeHandler.UpdateEmployee)
-		protectedEmployeeRoutes.DELETE("/:id", employeeHandler.DeleteEmployee)
+		protectedEmployeeRoutes.POST("", aboutHandler.CreateEmployee)
+		protectedEmployeeRoutes.PUT("/:id", aboutHandler.UpdateEmployee)
+		protectedEmployeeRoutes.DELETE("/:id", aboutHandler.DeleteEmployee)
 	}
 
 	// Timeline routes
 	timelineGroup := router.Group("/api/timeline")
 	timelineGroup.GET("", aboutHandler.GetTimelines)
-
 	protectedTimelineRoutes := timelineGroup.Group("").Use(authMiddleware.AdminRouteMiddleware())
 	{
 		protectedTimelineRoutes.POST("", aboutHandler.CreateTimeline)
