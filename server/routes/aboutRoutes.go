1a39dc0e8fc0a5af59069bd84f7b4e9d6ce6ed54
Sean Welch <seanwelch51199@icloud.com>
2025-02-17 09:02:41 +0000

    remove inti about

--- server/routes/aboutRoutes.go
+++ server/routes/aboutRoutes.go
@@ -1,28 +1,10 @@
 package routes
 
 import (
-	"database/sql"
-	"github.com/sean-david-welch/farmec-v2/server/lib"
-
 	"github.com/gin-gonic/gin"
 	"github.com/sean-david-welch/farmec-v2/server/handlers"
 	"github.com/sean-david-welch/farmec-v2/server/middleware"
-	"github.com/sean-david-welch/farmec-v2/server/repository"
-	"github.com/sean-david-welch/farmec-v2/server/services"
 )
-
-func InitAbout(router *gin.Engine, database *sql.DB, s3Client lib.S3Client, authMiddleware *middleware.AuthMiddlewareImpl, supplierCache *middleware.SupplierCache) {
-	// Initialize Employee resources
-	employeeRepo := repository.NewEmployeeRepo(database)
-	employeeService := services.NewEmployeeService(employeeRepo, s3Client, "Employees")
-	// Initialize Timeline resources
-	timelineRepo := repository.NewTimelineRepo(database)
-	timelineService := services.NewTimelineService(timelineRepo)
-
-	// init routes
-	aboutHandler := handlers.NewAboutHandler(employeeService, timelineService, authMiddleware, supplierCache)
-	AboutRoutes(router, aboutHandler, authMiddleware)
-}
 
 func AboutRoutes(router *gin.Engine, aboutHandler *handlers.AboutHandler, authMiddleware *middleware.AuthMiddlewareImpl) {
 	router.GET("/about", authMiddleware.ViewMiddleware(), aboutHandler.AboutView)
