6cbbe564360aad6b0ec764bb881cbf67de8fbdf5
Sean Welch <seanwelch51199@icloud.com>
2025-03-09 20:38:43 +0700

    format

--- server/sql/queries/suppliers.sql
+++ server/sql/queries/suppliers.sql
@@ -15,12 +15,38 @@ from Supplier
 order by created desc;
 
 -- name :GetSupplierWithResoueces :many
-select s.id, s.name, s.logo_image, s.marketing_image, s.description, s.social_facebook, s.social_twitter, s.social_instagram, s.social_youtube, s.social_linkedin, s.social_website, s.created, v.id, v.supplier_id, v.web_url, v.title, v.description, v.video_id, v.thumbnail_url, v.created, m.id, m.supplier_id, m.name, m.machine_image, m.description, m.machine_link, m.created
+select s.id,
+       s.name,
+       s.logo_image,
+       s.marketing_image,
+       s.description,
+       s.social_facebook,
+       s.social_twitter,
+       s.social_instagram,
+       s.social_youtube,
+       s.social_linkedin,
+       s.social_website,
+       s.created,
+       v.id,
+       v.supplier_id,
+       v.web_url,
+       v.title,
+       v.description,
+       v.video_id,
+       v.thumbnail_url,
+       v.created,
+       m.id,
+       m.supplier_id,
+       m.name,
+       m.machine_image,
+       m.description,
+       m.machine_link,
+       m.created
 from Supplier s
-left join
-    Video v on v.supplier_id = s.id
-left join
-    Machine m on m.supplier_id = s.id
+         left join
+     Video v on v.supplier_id = s.id
+         left join
+     Machine m on m.supplier_id = s.id
 order by s.created desc;
 
 -- name: GetSupplierByID :one
