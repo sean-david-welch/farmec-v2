efbb1ef9a4e73bc4a324cb01ef9550e06d0eaa62
Sean Welch <seanwelch51199@icloud.com>
2025-03-09 20:44:08 +0700

    seperate into different queries

--- server/sql/queries/suppliers.sql
+++ server/sql/queries/suppliers.sql
@@ -14,41 +14,20 @@ select id,
 from Supplier
 order by created desc;
 
--- name :GetSupplierWithResoueces :many
-select s.id,
-       s.name,
-       s.logo_image,
-       s.marketing_image,
-       s.description,
-       s.social_facebook,
-       s.social_twitter,
-       s.social_instagram,
-       s.social_youtube,
-       s.social_linkedin,
-       s.social_website,
-       s.created,
-       v.id,
-       v.supplier_id,
-       v.web_url,
-       v.title,
-       v.description,
-       v.video_id,
-       v.thumbnail_url,
-       v.created,
-       m.id,
-       m.supplier_id,
-       m.name,
-       m.machine_image,
-       m.description,
-       m.machine_link,
-       m.created
-from Supplier s
-         left join
-     Video v on v.supplier_id = s.id
-         left join
-     Machine m on m.supplier_id = s.id
-order by s.created desc;
+-- name: GetSuppliers :many
+SELECT id, name, logo_image, marketing_image, description, social_facebook, social_twitter, social_instagram, social_youtube, social_linkedin, social_website, created
+FROM Supplier
+ORDER BY created DESC;
 
+-- name: GetVideosBySupplierID :many
+SELECT id, supplier_id, web_url, title, description, video_id, thumbnail_url, created
+FROM Video
+WHERE supplier_id = ?;
+
+-- name: GetMachinesBySupplierID :many
+SELECT id, supplier_id, name, machine_image, description, machine_link, created
+FROM Machine
+WHERE supplier_id = ?;
 -- name: GetSupplierByID :one
 select id,
        name,
