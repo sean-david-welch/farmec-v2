300a8d5b3e30cbd7e06109e62c4c1b5455b0a53a
Sean Welch <seanwelch51199@icloud.com>
2025-03-09 13:52:11 +0700

    registration routes

--- server/routes/registrationRoutes.go
+++ server/routes/registrationRoutes.go
@@ -7,12 +7,14 @@ import (
 )
 
 func RegistrationRoutes(router *gin.Engine, handler *handlers.RegistrationHandler, authMiddleware *middleware.AuthMiddlewareImpl) {
+	router.GET("/registrations", authMiddleware.ViewMiddleware(), handler.RegistrationsView)
+	router.GET("/registrations/:id", authMiddleware.ViewMiddleware(), handler.RegistrationsDetailView)
 	registrationGroup := router.Group("/api/registrations")
-
-	registrationGroup.GET("", handler.GetRegistrations)
-	registrationGroup.GET("/:id", handler.GetRegistrationById)
-	registrationGroup.POST("", handler.CreateRegistration)
-
+	{
+		registrationGroup.GET("", handler.GetRegistrations)
+		registrationGroup.GET("/:id", handler.GetRegistrationById)
+		registrationGroup.POST("", handler.CreateRegistration)
+	}
 	protected := registrationGroup.Group("").Use(authMiddleware.AuthRouteMiddleware())
 	{
 		protected.PUT("/:id", handler.UpdateRegistration)
