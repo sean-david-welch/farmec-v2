e867c15f78970fb56acd16be87f3f4ac4d7bbfe1
Sean Welch <seanwelch51199@icloud.com>
2025-03-31 17:58:15 +0100

    add google

---
+++ server/static/js/google.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-5J4GB3RZ0N', {
+    send_page_view: true
+});
