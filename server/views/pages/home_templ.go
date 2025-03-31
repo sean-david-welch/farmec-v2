969beecf446df156100b609266a48c6ff6ee309b
Sean Welch <seanwelch51199@icloud.com>
2025-03-31 19:44:28 +0100

    remove

--- server/views/pages/home_templ.go
+++ server/views/pages/home_templ.go
@@ -58,10 +58,6 @@ func homeContent(carousels []types.Carousel) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templates.Displays().Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
 		templ_7745c5c3_Err = templates.Contact().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
