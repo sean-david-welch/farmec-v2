a548fda5006d2f180377708a5a977b28245dc128
Sean Welch <seanwelch51199@icloud.com>
2025-03-31 19:58:04 +0100

    fix

--- server/views/templates/contact_templ.go
+++ server/views/templates/contact_templ.go
@@ -33,7 +33,7 @@ func Contact() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section id=\"contact\"><h1 class=\"sectionHeading\">Contact Us:</h1><div class=\"contactSection\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section id=\"contact\"><h1 class=\"sectionHeading\">Contact Us:</h1><div class=\"contactSection\"><div class=\"infoSection\"><h1 class=\"subHeading\">Business Information:</h1><div class=\"info\"><div class=\"infoItem\">Opening Hours:<br><span class=\"infoItemText\">Monday - Friday: 9am - 5:30pm</span></div><div class=\"infoItem\">Telephone:<br><span class=\"infoItemText\"><a href=\"tel:01 825 9289\">01 825 9289</a></span></div><div class=\"infoItem\">International:<br><span class=\"infoItemText\"><a href=\"tel:+353 1 825 9289\">+353 1 825 9289</a></span></div><div class=\"infoItem\">Email:<br><span class=\"infoItemText\">Info@farmec.ie</span></div><div class=\"infoItem\">Address:<br><span class=\"infoItemText\">Clonross, Drumree, Co. Meath, A85PK30</span></div><div class=\"infoItem\"><div class=\"socialLinks\"><a class=\"socials\" href=\"https://www.facebook.com/FarmecIreland/\" target=\"_blank\" rel=\"noopener noreferrer\" aria-label=\"Visit our Facebook page\"><img src=\"/public/icons/facebook.svg\" alt=\"Facebook\" class=\"icon\"></a> <a class=\"socials\" href=\"https://twitter.com/farmec1?lang=en\" target=\"_blank\" rel=\"noopener noreferrer\" aria-label=\"Visit our Twitter page\"><img src=\"/public/icons/twitter.svg\" alt=\"Twitter\" class=\"icon\"></a></div></div></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -41,7 +41,7 @@ func Contact() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div class=\"infoSection\"><h1 class=\"subHeading\">Business Information:</h1><div class=\"info\"><div class=\"infoItem\">Opening Hours:<br><span class=\"infoItemText\">Monday - Friday: 9am - 5:30pm</span></div><div class=\"infoItem\">Telephone:<br><span class=\"infoItemText\"><a href=\"tel:01 825 9289\">01 825 9289</a></span></div><div class=\"infoItem\">International:<br><span class=\"infoItemText\"><a href=\"tel:+353 1 825 9289\">+353 1 825 9289</a></span></div><div class=\"infoItem\">Email:<br><span class=\"infoItemText\">Info@farmec.ie</span></div><div class=\"infoItem\">Address:<br><span class=\"infoItemText\">Clonross, Drumree, Co. Meath, A85PK30</span></div><div class=\"infoItem\"><div class=\"socialLinks\"><a class=\"socials\" href=\"https://www.facebook.com/FarmecIreland/\" target=\"_blank\" rel=\"noopener noreferrer\" aria-label=\"Visit our Facebook page\"><img src=\"/public/icons/facebook.svg\" alt=\"Facebook\" class=\"icon\"></a> <a class=\"socials\" href=\"https://twitter.com/farmec1?lang=en\" target=\"_blank\" rel=\"noopener noreferrer\" aria-label=\"Visit our Twitter page\"><img src=\"/public/icons/twitter.svg\" alt=\"Twitter\" class=\"icon\"></a></div></div></div></div></div></section>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div></section>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
