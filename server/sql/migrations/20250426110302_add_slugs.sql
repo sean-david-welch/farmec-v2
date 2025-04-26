9f914142680f2e6283c0b642933dc0e2d3831b16
Sean Welch <seanwelch51199@icloud.com>
2025-04-26 12:12:38 +0100

    migrations for all

--- server/sql/migrations/20250426110302_add_slugs.sql
+++ server/sql/migrations/20250426110302_add_slugs.sql
@@ -1,10 +1,66 @@
 -- +goose Up
 -- +goose StatementBegin
--- Add the slug column
+-- Add the slug column to all tables
 ALTER TABLE Supplier ADD COLUMN slug TEXT;
+ALTER TABLE Machine ADD COLUMN slug TEXT;
+ALTER TABLE Product ADD COLUMN slug TEXT;
+ALTER TABLE Blog ADD COLUMN slug TEXT;
+ALTER TABLE SpareParts ADD COLUMN slug TEXT;
 
 -- Update existing records to populate slugs
-update Supplier
+UPDATE Supplier
+SET slug = LOWER(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(name, ' ', '-'),
+                    '.', ''),
+                '&', 'and'),
+            ',', ''),
+        '--', '-')
+    );
+
+UPDATE Machine
+SET slug = LOWER(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(name, ' ', '-'),
+                    '.', ''),
+                '&', 'and'),
+            ',', ''),
+        '--', '-')
+    );
+
+UPDATE Product
+SET slug = LOWER(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(name, ' ', '-'),
+                    '.', ''),
+                '&', 'and'),
+            ',', ''),
+        '--', '-')
+    );
+
+UPDATE Blog
+SET slug = LOWER(
+    REPLACE(
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(title, ' ', '-'),
+                    '.', ''),
+                '&', 'and'),
+            ',', ''),
+        '--', '-')
+    );
+
+UPDATE SpareParts
 SET slug = LOWER(
     REPLACE(
         REPLACE(
@@ -21,4 +77,8 @@ SET slug = LOWER(
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE Supplier DROP COLUMN slug;
+ALTER TABLE Machine DROP COLUMN slug;
+ALTER TABLE Product DROP COLUMN slug;
+ALTER TABLE Blog DROP COLUMN slug;
+ALTER TABLE SpareParts DROP COLUMN slug;
 -- +goose StatementEnd
