8d109856651bbdc74f4e82da8afafa93e60ecfcd
Sean Welch <seanwelch51199@icloud.com>
2025-10-25 11:46:36 -0400

    add controller

--- farmec/lib/farmec_web/controllers/page_controller.ex
+++ farmec/lib/farmec_web/controllers/page_controller.ex
@@ -2,8 +2,23 @@ defmodule FarmecWeb.PageController do
   use FarmecWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    # Mock carousel images (replace with DB query later)
+    carousel_images = [
+      "/images/carousel1.jpg",
+      "/images/carousel2.jpg",
+      "/images/carousel3.jpg"
+    ]
+
+    # Get display info from our static module
+    specials = FarmecWeb.DisplayInfo.specials_items()
+    stats = FarmecWeb.DisplayInfo.stats_items()
+
+    render(conn, :home,
+      layout: false,
+      carousel_images: carousel_images,
+      specials: specials,
+      stats: stats,
+      page_title: "Home - Farmec Ireland"
+    )
   end
 end
