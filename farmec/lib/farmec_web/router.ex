94d99ec1bb1787050a377b32253702d61d8454b6
Sean Welch <seanwelch51199@icloud.com>
2025-10-31 16:11:43 -0400

    fix router

--- farmec/lib/farmec_web/router.ex
+++ farmec/lib/farmec_web/router.ex
@@ -17,7 +17,7 @@ defmodule FarmecWeb.Router do
   scope "/", FarmecWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", PageLive
   end
 
   # Other scopes may use custom stacks.
