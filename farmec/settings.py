d42a923a1d098f50539a1bab251d21de8a0012dc
Sean Welch <seanwelch51199@icloud.com>
2026-01-25 15:23:49 +0000

    edit settings

--- farmec/settings.py
+++ farmec/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'farmec.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'database', 'database.db'),
     }
 }
 
