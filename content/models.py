39ed71130b7d205810428b21de0b4a592208200a
Sean Welch <seanwelch51199@icloud.com>
2026-01-25 15:34:12 +0000

    add models to content

--- content/models.py
+++ content/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    date = models.TextField(blank=True, null=True)
+    main_image = models.TextField(blank=True, null=True)
+    subheading = models.TextField(blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+    slug = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Blog'
+
+
+class Carousel(models.Model):
+    id = models.TextField(primary_key=True)
+    name = models.TextField()
+    image = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Carousel'
+
+
+class Exhibition(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    date = models.TextField(blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
+    info = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Exhibition'
+
+
+class Timeline(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    date = models.TextField(blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Timeline'
