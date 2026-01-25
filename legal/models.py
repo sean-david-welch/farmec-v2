39e2c2fc71da50c9b56f5f63d565e9db12f40463
Sean Welch <seanwelch51199@icloud.com>
2026-01-25 15:34:18 +0000

    add models to legal

--- legal/models.py
+++ legal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Privacy(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    body = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Privacy'
+
+
+class Terms(models.Model):
+    id = models.TextField(primary_key=True)
+    title = models.TextField()
+    body = models.TextField(blank=True, null=True)
+    created = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Terms'
