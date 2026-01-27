f4bab7e8a0f76e321d7efb4af862815e137adbac
Sean Welch <seanwelch51199@icloud.com>
2026-01-27 23:04:32 +0000

    supplier views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Supplier, SupplierQuerySet
+
+
+class SupplierListView(ListView):
+    model: type[Supplier] = Supplier
+    template_name: str = 'catalog/supplier_list.html'
+    context_object_name: str = 'suppliers'
+    queryset: SupplierQuerySet = Supplier.objects.publish().order_by('-created')
+
+
+class SupplierDetailView(DetailView):
+    model: type[Supplier] = Supplier
+    template_name: str = 'catalog/supplier_detail.html'
+    context_object_name: str = 'supplier'
+    slug_field: str = 'slug'
+    slug_url_kwarg: str = 'slug'
+    queryset: SupplierQuerySet = Supplier.objects.publish()
