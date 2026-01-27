d43a68e04cfea94bc6b3a18fccf0c674f0b034a8
Sean Welch <seanwelch51199@icloud.com>
2026-01-27 23:05:00 +0000

    machine vies

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 
-from .models import Supplier, SupplierQuerySet
+from .models import Supplier, SupplierQuerySet, Machine, MachineQuerySet
 
 
 class SupplierListView(ListView):
@@ -17,3 +17,19 @@ class SupplierDetailView(DetailView):
     slug_field: str = 'slug'
     slug_url_kwarg: str = 'slug'
     queryset: SupplierQuerySet = Supplier.objects.publish()
+
+
+class MachineListView(ListView):
+    model: type[Machine] = Machine
+    template_name: str = 'catalog/machine_list.html'
+    context_object_name: str = 'machines'
+    queryset: MachineQuerySet = Machine.objects.publish().order_by('-created')
+
+
+class MachineDetailView(DetailView):
+    model: type[Machine] = Machine
+    template_name: str = 'catalog/machine_detail.html'
+    context_object_name: str = 'machine'
+    slug_field: str = 'slug'
+    slug_url_kwarg: str = 'slug'
+    queryset: MachineQuerySet = Machine.objects.publish()
