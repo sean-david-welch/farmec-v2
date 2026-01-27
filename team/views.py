f595c5cead221f1efd3a4b430b6ec5ebcc3b5cc1
Sean Welch <seanwelch51199@icloud.com>
2026-01-27 23:08:41 +0000

    employee list view

--- team/views.py
+++ team/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Employee, EmployeeQuerySet
+
+
+class EmployeeListView(ListView):
+    model: type[Employee] = Employee
+    template_name: str = 'team/employee_list.html'
+    context_object_name: str = 'employees'
+    queryset: EmployeeQuerySet = Employee.objects.publish().order_by('-created')
