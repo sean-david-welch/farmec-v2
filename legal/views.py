02c201dc9557bdde12b057dada8c515af42be44c
Sean Welch <seanwelch51199@icloud.com>
2026-01-27 23:07:58 +0000

    legal views

--- legal/views.py
+++ legal/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Privacy, PrivacyQuerySet, Terms, TermsQuerySet
+
+
+class PrivacyListView(ListView):
+    model: type[Privacy] = Privacy
+    template_name: str = 'legal/privacy_list.html'
+    context_object_name: str = 'privacy'
+    queryset: PrivacyQuerySet = Privacy.objects.publish().order_by('-created')
+
+
+class TermsListView(ListView):
+    model: type[Terms] = Terms
+    template_name: str = 'legal/terms_list.html'
+    context_object_name: str = 'terms'
+    queryset: TermsQuerySet = Terms.objects.publish().order_by('-created')
