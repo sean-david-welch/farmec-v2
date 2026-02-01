6ea336c74faddc9f5e9860b8cb337a906853bf34
Sean Welch <seanwelch51199@icloud.com>
2026-02-01 22:35:58 +0000

    allowed host

--- farmec/settings.py
+++ farmec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s^+f^9tux8*a&&^%3evn@nfradmm)k16ea$q^=8wk12cfa_1=x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
