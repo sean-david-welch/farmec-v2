80d37848803a68ffef5d647aa623bae67ac9daca
Sean Welch <seanwelch51199@icloud.com>
2026-02-02 21:50:43 +0000

    admin catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from catalog.models import Supplier, Machine, Product, Spareparts, Lineitems, Video
 
-# Register your models here.
+@admin.register(Supplier)
+class SupplierAdmin(ModelAdmin):
+    list_display = ("name", "id", "slug")
+    search_fields = ("name", "description")
+
+
+@admin.register(Machine)
+class MachineAdmin(ModelAdmin):
+    list_display = ("name", "supplier", "slug")
+    search_fields = ("name", "description")
+    list_filter = ("supplier",)
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ("name", "machine", "slug")
+    search_fields = ("name", "description")
+
+
+@admin.register(Spareparts)
+class SparepartsAdmin(ModelAdmin):
+    list_display = ("name", "supplier", "slug")
+    search_fields = ("name",)
+
+
+@admin.register(Lineitems)
+class LineitemsAdmin(ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name",)
+
+
+@admin.register(Video)
+class VideoAdmin(ModelAdmin):
+    list_display = ("title", "supplier", "video_id")
+    search_fields = ("title", "description")
