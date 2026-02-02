4b42b1575973c728e9abf443ef058f4d50759feb
Sean Welch <seanwelch51199@icloud.com>
2026-02-02 21:50:59 +0000

    admin support

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Warrantyclaim, Partsrequired, Machineregistration
 
-# Register your models here.
+@admin.register(Warrantyclaim)
+class WarrantyclaimAdmin(ModelAdmin):
+    list_display = ("owner_name", "machine_model", "serial_number", "install_date")
+    search_fields = ("owner_name", "machine_model", "serial_number", "dealer")
+    list_filter = ("install_date", "dealer")
+
+
+@admin.register(Partsrequired)
+class PartsrequiredAdmin(ModelAdmin):
+    list_display = ("part_number", "quantity_needed", "warranty")
+    search_fields = ("part_number", "invoice_number")
+
+
+@admin.register(Machineregistration)
+class MachineregistrationAdmin(ModelAdmin):
+    list_display = ("owner_name", "machine_model", "serial_number", "date")
+    search_fields = ("owner_name", "machine_model", "serial_number", "dealer_name")
+    list_filter = ("date", "dealer_name")
