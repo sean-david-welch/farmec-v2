2b14a007146a0506cff280c2aca60b8a01a593b3
Sean Welch <seanwelch51199@icloud.com>
2026-02-02 21:50:47 +0000

    content

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Blog, Carousel, Exhibition, Timeline
 
-# Register your models here.
+@admin.register(Blog)
+class BlogAdmin(ModelAdmin):
+    list_display = ("title", "date", "slug")
+    search_fields = ("title", "subheading", "body")
+    list_filter = ("date",)
+
+
+@admin.register(Carousel)
+class CarouselAdmin(ModelAdmin):
+    list_display = ("name", "id")
+    search_fields = ("name",)
+
+
+@admin.register(Exhibition)
+class ExhibitionAdmin(ModelAdmin):
+    list_display = ("title", "date", "location")
+    search_fields = ("title", "location", "info")
+    list_filter = ("date",)
+
+
+@admin.register(Timeline)
+class TimelineAdmin(ModelAdmin):
+    list_display = ("title", "date")
+    search_fields = ("title", "body")
+    list_filter = ("date",)
