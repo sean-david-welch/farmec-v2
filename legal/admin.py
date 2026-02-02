30d965996b797b44b6019e33e510364bd0da5f93
Sean Welch <seanwelch51199@icloud.com>
2026-02-02 21:50:51 +0000

    legal

--- legal/admin.py
+++ legal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import Privacy, Terms
 
-# Register your models here.
+@admin.register(Privacy)
+class PrivacyAdmin(ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(Terms)
+class TermsAdmin(ModelAdmin):
+    list_display = ("title",)
