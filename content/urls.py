c3839dbb34727af36e8b3d420e6fae3ceafef9b0
Sean Welch <seanwelch51199@icloud.com>
2026-02-02 21:18:19 +0000

    fix urls

--- content/urls.py
+++ content/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name: str = 'content'
 
 urlpatterns: list = [
+    path('', views.HomeView.as_view(), name='home'),
     path('blog/', views.BlogListView.as_view(), name='blog_list'),
     path('blog/<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('carousel/', views.CarouselListView.as_view(), name='carousel_list'),
     path('exhibitions/', views.ExhibitionListView.as_view(), name='exhibition_list'),
     path('timeline/', views.TimelineListView.as_view(), name='timeline_list'),
 ]
