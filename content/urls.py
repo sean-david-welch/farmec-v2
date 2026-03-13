29f36448da5c62d67d3a9a41508e780e5ebe64a6
Sean Welch <seanwelch51199@icloud.com>
2026-03-12 23:34:05 -0400

    update blog url

--- content/urls.py
+++ content/urls.py
@@ -7,7 +7,7 @@ app_name: str = 'content'
 urlpatterns: list = [
     path('', views.HomeView.as_view(), name='home'),
     path('blog/', views.BlogListView.as_view(), name='blog_list'),
-    path('blog/<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<str:id>/', views.BlogDetailView.as_view(), name='blog_detail'),
     path('exhibitions/', views.ExhibitionListView.as_view(), name='exhibition_list'),
     path('timeline/', views.TimelineListView.as_view(), name='timeline_list'),
 ]
