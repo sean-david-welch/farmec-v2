d478ecda315675670136be530bd91849eef67f04
Sean Welch <seanwelch51199@icloud.com>
2026-03-12 23:40:21 -0400

    next page

--- farmec/urls.py
+++ farmec/urls.py
@@ -27,7 +27,7 @@ def server_error(request, exception=None):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='pages/login.html', next_page='/'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('catalog.urls')),
     path('', include('content.urls')),
