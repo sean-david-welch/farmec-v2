09f47c6dcd450df61183a7c8cdeb4af947e290bc
Sean Welch <seanwelch51199@icloud.com>
2026-03-12 21:58:35 -0400

    remove url

--- legal/urls.py
+++ legal/urls.py
@@ -6,5 +6,4 @@ app_name: str = 'legal'
 
 urlpatterns: list = [
     path('privacy/', views.PrivacyListView.as_view(), name='privacy_list'),
-    path('terms/', views.TermsListView.as_view(), name='terms_list'),
 ]
