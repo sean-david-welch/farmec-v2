f1f1c0676e3f9047a99dc2b063230c3c4aae3f33
Sean Welch <seanwelch51199@icloud.com>
2026-03-12 21:58:40 -0400

    update views

--- legal/views.py
+++ legal/views.py
@@ -5,13 +5,11 @@ from .models import Privacy, PrivacyQuerySet, Terms, TermsQuerySet
 
 class PrivacyListView(ListView):
     model: type[Privacy] = Privacy
-    template_name: str = 'legal/privacy_list.html'
-    context_object_name: str = 'privacy'
+    template_name: str = 'legal/policies.html'
+    context_object_name: str = 'policies'
     queryset: PrivacyQuerySet = Privacy.objects.publish().order_by('-created')
 
-
-class TermsListView(ListView):
-    model: type[Terms] = Terms
-    template_name: str = 'legal/terms_list.html'
-    context_object_name: str = 'terms'
-    queryset: TermsQuerySet = Terms.objects.publish().order_by('-created')
+    def get_context_data(self, **kwargs) -> dict[str, PrivacyQuerySet| TermsQuerySet]:
+        context = super().get_context_data(**kwargs)
+        context['terms'] = Terms.objects.publish().order_by('-created')
+        return context
