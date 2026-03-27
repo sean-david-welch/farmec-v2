308b00ed41e2a270e45972ac0bd394ec8ff7084f
Sean Welch <seanwelch51199@icloud.com>
2026-03-27 19:01:54 -0400

    add debug urls

--- farmec/urls.py
+++ farmec/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -35,6 +36,10 @@ urlpatterns = [
     path('', include('team.urls')),
     path('', include('support.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
 
 # Error handlers
 handler404 = page_not_found
