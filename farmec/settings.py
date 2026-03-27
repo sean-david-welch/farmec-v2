3c19e5903260a77acc74b0b6f0300c5c99ea0552
Sean Welch <seanwelch51199@icloud.com>
2026-03-27 19:01:47 -0400

    add debug toolbar to settings.py

--- farmec/settings.py
+++ farmec/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party
+    'debug_toolbar',
     'django_htmx',
     'template_partials',
     # Model apps
@@ -59,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
