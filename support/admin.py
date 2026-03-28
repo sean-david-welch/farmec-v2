06fff5ae5af743912c3ca39224e58a3e26ad9939
Sean Welch <seanwelch51199@icloud.com>
2026-03-28 16:36:56 -0400

    edit admin support

--- support/admin.py
+++ support/admin.py
@@ -1,28 +1,44 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, StackedInline
 
 from .forms import WarrantyclaimForm, PartsrequiredForm, MachineregistrationForm
 from .models import Warrantyclaim, Partsrequired, Machineregistration
+
+BASE_READONLY = ('uid', 'created', 'modified')
+
+
+class PartsrequiredInline(StackedInline):
+    model = Partsrequired
+    form = PartsrequiredForm
+    extra = 0
+    readonly_fields = BASE_READONLY
 
 
 @admin.register(Warrantyclaim)
 class WarrantyclaimAdmin(ModelAdmin):
     form = WarrantyclaimForm
-    list_display = ("owner_name", "machine_model", "serial_number", "install_date")
-    search_fields = ("owner_name", "machine_model", "serial_number", "dealer")
-    list_filter = ("install_date", "dealer")
+    inlines = [PartsrequiredInline]
+    list_display = ('owner_name', 'dealer', 'machine_model', 'serial_number', 'failure_date', 'completed_by')
+    search_fields = ('owner_name', 'machine_model', 'serial_number', 'dealer')
+    list_filter = ('dealer',)
+    readonly_fields = BASE_READONLY
+    ordering = ('-created',)
 
 
 @admin.register(Partsrequired)
 class PartsrequiredAdmin(ModelAdmin):
     form = PartsrequiredForm
-    list_display = ("part_number", "quantity_needed", "warranty")
-    search_fields = ("part_number", "invoice_number")
+    list_display = ('part_number', 'quantity_needed', 'invoice_number', 'warranty')
+    search_fields = ('part_number', 'invoice_number')
+    readonly_fields = BASE_READONLY
+    ordering = ('warranty',)
 
 
 @admin.register(Machineregistration)
 class MachineregistrationAdmin(ModelAdmin):
     form = MachineregistrationForm
-    list_display = ("owner_name", "machine_model", "serial_number", "date")
-    search_fields = ("owner_name", "machine_model", "serial_number", "dealer_name")
-    list_filter = ("date", "dealer_name")
+    list_display = ('owner_name', 'dealer_name', 'machine_model', 'serial_number', 'date', 'completed_by')
+    search_fields = ('owner_name', 'machine_model', 'serial_number', 'dealer_name')
+    list_filter = ('dealer_name',)
+    readonly_fields = BASE_READONLY
+    ordering = ('-created',)
