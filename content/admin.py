b0f7d237cdab42ac81064ecda7b785fea5f93f86
Sean Welch <seanwelch51199@icloud.com>
2026-03-28 16:36:50 -0400

    edit admin content

--- content/admin.py
+++ content/admin.py
@@ -4,33 +4,42 @@ from unfold.admin import ModelAdmin
 from .forms import BlogForm, CarouselForm, ExhibitionForm, TimelineForm
 from .models import Blog, Carousel, Exhibition, Timeline
 
+BASE_READONLY = ('uid', 'created', 'modified')
+
 
 @admin.register(Blog)
 class BlogAdmin(ModelAdmin):
     form = BlogForm
-    list_display = ("title", "date", "slug")
-    search_fields = ("title", "subheading", "body")
-    list_filter = ("date",)
+    list_display = ('title', 'date', 'publish')
+    search_fields = ('title', 'subheading', 'body')
+    list_filter = ('date',)
+    readonly_fields = BASE_READONLY
+    ordering = ('-date',)
 
 
 @admin.register(Carousel)
 class CarouselAdmin(ModelAdmin):
     form = CarouselForm
-    list_display = ("name", "id")
-    search_fields = ("name",)
+    list_display = ('name', 'order', 'publish')
+    search_fields = ('name',)
+    readonly_fields = BASE_READONLY
+    ordering = ('order',)
 
 
 @admin.register(Exhibition)
 class ExhibitionAdmin(ModelAdmin):
     form = ExhibitionForm
-    list_display = ("title", "date", "location")
-    search_fields = ("title", "location", "info")
-    list_filter = ("date",)
+    list_display = ('title', 'date', 'location', 'publish')
+    search_fields = ('title', 'location', 'info')
+    list_filter = ('date',)
+    readonly_fields = BASE_READONLY
+    ordering = ('-date',)
 
 
 @admin.register(Timeline)
 class TimelineAdmin(ModelAdmin):
     form = TimelineForm
-    list_display = ("title", "date")
-    search_fields = ("title", "body")
-    list_filter = ("date",)
+    list_display = ('title', 'date', 'publish')
+    search_fields = ('title', 'body')
+    readonly_fields = BASE_READONLY
+    ordering = ('-date',)
