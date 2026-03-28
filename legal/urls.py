3a08146b93b26e59e9036f5dbce6c12a7b761088
Sean Welch <seanwelch51199@icloud.com>
2026-03-27 20:18:37 -0400

    terms list view

--- legal/urls.py
+++ legal/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name: str = 'legal'
 
 urlpatterns: list = [
-    path('privacy/', views.PrivacyListView.as_view(), name='privacy_list'),
+    path('privacy/', views.TermsListView.as_view(), name='privacy_list'),
 ]
--- legal/views.py
+++ legal/views.py
@@ -3,13 +3,14 @@ from django.views.generic import ListView
 from .models import Privacy, PrivacyQuerySet, Terms, TermsQuerySet
 
 
-class PrivacyListView(ListView):
+class TermsListView(ListView):
     model: type[Privacy] = Privacy
     template_name: str = 'legal/policies.html'
     context_object_name: str = 'policies'
     queryset: PrivacyQuerySet = Privacy.objects.publish().order_by('-created')
+    terms_queryset: TermsQuerySet = Terms.objects.publish().order_by('-created')
 
     def get_context_data(self, **kwargs) -> dict[str, PrivacyQuerySet| TermsQuerySet]:
         context = super().get_context_data(**kwargs)
-        context['terms'] = Terms.objects.publish().order_by('-created')
+        context.update(terms=self.terms_queryset)
         return context
