ee78314ae3d1e88ba8e4271606baebeabebb18d7
Sean Welch <seanwelch51199@icloud.com>
2026-03-28 16:37:01 -0400

    edit admin team

--- team/admin.py
+++ team/admin.py
@@ -4,9 +4,13 @@ from unfold.admin import ModelAdmin
 from .forms import EmployeeForm
 from .models import Employee
 
+BASE_READONLY = ('uid', 'created', 'modified')
+
 
 @admin.register(Employee)
 class EmployeeAdmin(ModelAdmin):
     form = EmployeeForm
-    list_display = ("name", "role", "email")
-    search_fields = ("name", "role", "email")
+    list_display = ('name', 'role', 'email', 'publish')
+    search_fields = ('name', 'role', 'email')
+    readonly_fields = BASE_READONLY
+    ordering = ('name',)
