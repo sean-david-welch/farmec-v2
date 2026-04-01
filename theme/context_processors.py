7e50a675bcbb3aaa5384db6fac27d07b547ec9be
Sean Welch <seanwelch51199@icloud.com>
2026-03-31 23:02:47 -0400

    add context_processors.py

--- theme/context_processors.py
+++ theme/context_processors.py
@@ -3,7 +3,7 @@ from catalog.models import Supplier, SupplierQuerySet
 
 def suppliers(request):
     """Make suppliers available to all templates."""
-    suppliers_list: SupplierQuerySet = Supplier.objects.publish().only('id', 'name').order_by('-created')
+    suppliers_list: SupplierQuerySet = Supplier.objects.publish().only('id', 'name', 'slug').order_by('-created')
     return {
         'suppliers': suppliers_list,
     }
