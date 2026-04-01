17c18908aab1282229fe55204a0a5da3c92b0436
Sean Welch <seanwelch51199@icloud.com>
2026-03-31 22:25:35 -0400

    add redirect urls

--- farmec/urls.py
+++ farmec/urls.py
@@ -28,6 +28,19 @@ def server_error(request, exception=None):
     return render(request, 'error.html', status=500)
 
 urlpatterns = [
+    path('supplier', RedirectView.as_view(url='/suppliers/', permanent=True)),
+    path('supplier/', RedirectView.as_view(url='/suppliers/', permanent=True)),
+    path('about', RedirectView.as_view(url='/team/', permanent=True)),
+    path('about/', RedirectView.as_view(url='/team/', permanent=True)),
+    path('about/policies', RedirectView.as_view(url='/privacy/', permanent=True)),
+    path('about/policies/', RedirectView.as_view(url='/privacy/', permanent=True)),
+    path('blogs', RedirectView.as_view(url='/blog/', permanent=True)),
+    path('blogs/', RedirectView.as_view(url='/blog/', permanent=True)),
+    path('blog/exhibitions', RedirectView.as_view(url='/exhibitions/', permanent=True)),
+    path('exhibitions', RedirectView.as_view(url='/exhibitions/', permanent=True)),
+    path('spareparts', RedirectView.as_view(url='/spareparts/', permanent=True)),
+    path('suppliers', RedirectView.as_view(url='/suppliers/', permanent=True)),
+
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.svg', permanent=True)),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='pages/login.html', next_page='/'), name='login'),
