aeecf89ac45097a4be193a20d7799b2f3ce4a77d
Sean Welch <seanwelch51199@icloud.com>
2026-04-02 22:35:27 -0400

    register signal

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_save
+
+from catalog.models import Video
 
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
+
+    def ready(self) -> None:
+        from catalog.signals import populate_youtube_metadata
+        super().ready()
+        pre_save.connect(receiver=populate_youtube_metadata, sender=Video, dispatch_uid='populate_document_metadata')
