af69c0dff91b01701b2638977b8fdea37a346e95
Sean Welch <seanwelch51199@icloud.com>
2026-04-02 22:41:50 -0400

    fix import

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,13 +1,12 @@
 from django.apps import AppConfig
 from django.db.models.signals import pre_save
 
-from catalog.models import Video
-
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
 
     def ready(self) -> None:
+        from catalog.models import Video
         from catalog.signals import populate_youtube_metadata
         super().ready()
-        pre_save.connect(receiver=populate_youtube_metadata, sender=Video, dispatch_uid='populate_document_metadata')
+        pre_save.connect(receiver=populate_youtube_metadata, sender=Video, dispatch_uid='populate_youtube_metadata')
