846743b085eed73a16ab667c706a22c41a2d081a
Sean Welch <seanwelch51199@icloud.com>
2026-04-04 19:34:36 -0400

    context_processors.py

--- theme/context_processors.py
+++ theme/context_processors.py
@@ -1,9 +1,15 @@
+from googleapiclient.http import HttpRequest
+
 from catalog.models import Supplier, SupplierQuerySet
 
 
-def suppliers(request):
-    """Make suppliers available to all templates."""
-    suppliers_list: SupplierQuerySet = Supplier.objects.publish().only('id', 'name', 'slug').order_by('-created')
+def suppliers(request: HttpRequest) -> dict[str, SupplierQuerySet]:
+    """
+    Make suppliers available to all templates including in the Navbar context
+
+    :param request: The HttpRequest object
+    """
+    suppliers_list: SupplierQuerySet = Supplier.objects.publish().only('id', 'name', 'slug').order_by('order')
     return {
         'suppliers': suppliers_list,
     }
