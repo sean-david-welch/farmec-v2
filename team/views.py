890967feb517a226f70b96a7106efef79b2d466e
Sean Welch <seanwelch51199@icloud.com>
2026-04-04 22:27:40 -0400

    fix order

--- team/views.py
+++ team/views.py
@@ -9,7 +9,7 @@ class EmployeeListView(ListView):
     template_name: str = 'pages/about.html'
     context_object_name: str = 'employees'
     queryset: EmployeeQuerySet = Employee.objects.publish().order_by('order')
-    timeline_queryset: TimelineQuerySet = Timeline.objects.publish().order_by('date')
+    timeline_queryset: TimelineQuerySet = Timeline.objects.publish().order_by('order')
 
     def get_context_data(self, **kwargs) -> dict[str, EmployeeQuerySet | TimelineQuerySet]:
         context = super().get_context_data(**kwargs)
