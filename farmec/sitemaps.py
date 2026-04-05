64ddab80d3a43324d5b462988337e46559f602d2
Sean Welch <seanwelch51199@icloud.com>
2026-04-04 21:20:07 -0400

    add type annotations

--- farmec/sitemaps.py
+++ farmec/sitemaps.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.contrib.sitemaps import Sitemap
+from django.db.models import QuerySet
 from django.urls import reverse
 
 from catalog.models import Supplier, Machine, Spareparts
@@ -6,76 +9,76 @@ from content.models import Blog, Exhibition
 
 
 class SupplierSitemap(Sitemap):
-    changefreq = 'monthly'
-    priority = 0.9
+    changefreq: str = 'monthly'
+    priority: float = 0.9
 
-    def items(self):
+    def items(self) -> QuerySet[Supplier]:
         return Supplier.objects.publish().filter(slug__isnull=False)
 
-    def lastmod(self, obj):
+    def lastmod(self, obj: Supplier) -> datetime:
         return obj.modified
 
-    def location(self, obj):
+    def location(self, obj: Supplier) -> str:
         return f'/suppliers/{obj.slug}/'
 
 
 class MachineSitemap(Sitemap):
-    changefreq = 'monthly'
-    priority = 0.8
+    changefreq: str = 'monthly'
+    priority: float = 0.8
 
-    def items(self):
+    def items(self) -> QuerySet[Machine]:
         return Machine.objects.publish().filter(slug__isnull=False)
 
-    def lastmod(self, obj):
+    def lastmod(self, obj: Machine) -> datetime:
         return obj.modified
 
-    def location(self, obj):
+    def location(self, obj: Machine) -> str:
         return f'/machines/{obj.slug}/'
 
 
 class SparePartsSitemap(Sitemap):
-    changefreq = 'monthly'
-    priority = 0.8
+    changefreq: str = 'monthly'
+    priority: float = 0.8
 
-    def items(self):
+    def items(self) -> QuerySet[Spareparts]:
         return Spareparts.objects.publish().filter(slug__isnull=False)
 
-    def lastmod(self, obj):
+    def lastmod(self, obj: Spareparts) -> datetime:
         return obj.modified
 
-    def location(self, obj):
+    def location(self, obj: Spareparts) -> str:
         return f'/spareparts/{obj.slug}/'
 
 
 class BlogSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 0.7
+    changefreq: str = 'weekly'
+    priority: float = 0.7
 
-    def items(self):
+    def items(self) -> QuerySet[Blog]:
         return Blog.objects.publish()
 
-    def lastmod(self, obj):
+    def lastmod(self, obj: Blog) -> datetime:
         return obj.modified
 
-    def location(self, obj):
+    def location(self, obj: Blog) -> str:
         return f'/blog/{obj.id}/'
 
 
 class ExhibitionSitemap(Sitemap):
-    changefreq = 'monthly'
-    priority = 0.6
+    changefreq: str = 'monthly'
+    priority: float = 0.6
 
-    def items(self):
+    def items(self) -> QuerySet[Exhibition]:
         return Exhibition.objects.publish()
 
-    def lastmod(self, obj):
+    def lastmod(self, obj: Exhibition) -> datetime:
         return obj.modified
 
-    def location(self, obj):
-        return f'/exhibitions/'
+    def location(self, obj: Exhibition) -> str:
+        return '/exhibitions/'
 
 
-STATIC_URLS = {
+STATIC_URLS: dict[str, float] = {
     'content:home': 1.0,
     'catalog:supplier_list': 0.9,
     'catalog:machine_list': 0.8,
@@ -88,19 +91,19 @@ STATIC_URLS = {
 
 
 class StaticSitemap(Sitemap):
-    changefreq = 'monthly'
+    changefreq: str = 'monthly'
 
-    def items(self):
+    def items(self) -> list[str]:
         return list(STATIC_URLS.keys())
 
-    def priority(self, item):
+    def priority(self, item: str) -> float:
         return STATIC_URLS[item]
 
-    def location(self, item):
+    def location(self, item: str) -> str:
         return reverse(item)
 
 
-sitemaps = {
+sitemaps: dict[str, type[Sitemap]] = {
     'suppliers': SupplierSitemap,
     'machines': MachineSitemap,
     'spareparts': SparePartsSitemap,
