5b1ae82952e16de6e13caa8fc7690a3e81fa1faa
Sean Welch <seanwelch51199@icloud.com>
2026-05-12 21:36:40 +0100

    add product detail url

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,6 +9,7 @@ urlpatterns: list = [
     path('suppliers/<slug:slug>/', views.SupplierDetailView.as_view(), name='supplier_detail'),
     path('machines/', views.MachineListView.as_view(), name='machine_list'),
     path('machines/<slug:slug>/', views.MachineDetailView.as_view(), name='machine_detail'),
+    path('products/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('spareparts/', views.SparePartsIndexView.as_view(), name='spareparts'),
     path('spareparts/<slug:slug>/', views.SparePartsListView.as_view(), name='spareparts_detail'),
 ]
