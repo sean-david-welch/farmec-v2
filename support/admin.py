e6048c9db8037e0030c8c2fd156307499fa3edb7
Sean Welch <seanwelch51199@icloud.com>
2026-05-16 13:15:02 +0100

    add admin action to download claim images

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,8 @@
+import io
+import os
+import re
+import zipfile
+
 from django.contrib import admin
 from django.http import HttpRequest, HttpResponse
 from unfold.admin import ModelAdmin, TabularInline
@@ -28,7 +33,7 @@ class WarrantyclaimAdmin(ModelAdmin):
     form = WarrantyclaimForm
     inlines = [PartsrequiredInline, WarrantyImageInline]
     actions = ['download_pdf']
-    actions_detail = ['download_pdf_detail']
+    actions_detail = ['download_pdf_detail', 'download_images_detail']
     list_display = ('owner_name', 'dealer', 'machine_model', 'serial_number', 'failure_date', 'parts_count', 'completed_by')
     search_fields = ('owner_name', 'machine_model', 'serial_number', 'dealer')
     list_filter = ('dealer',)
@@ -59,6 +64,20 @@ class WarrantyclaimAdmin(ModelAdmin):
     @action(description='Download PDF', icon='download')
     def download_pdf_detail(self, request: HttpRequest, object_id: str) -> HttpResponse:
         return self.download_pdf(self, request, Warrantyclaim.objects.filter(pk=object_id))
+
+    @action(description='Download Images', icon='photo_library')
+    def download_images_detail(self, request: HttpRequest, object_id: str) -> HttpResponse:
+        claim: Warrantyclaim = Warrantyclaim.objects.prefetch_related('images').get(pk=object_id)
+        slug: str = re.sub(r'\W+', '_', f'{claim.owner_name}_{claim.machine_model}').strip('_').lower()
+        buffer: io.BytesIO = io.BytesIO()
+        with zipfile.ZipFile(buffer, 'w') as zf:
+            for i, image in enumerate(claim.images.all(), start=1):
+                ext: str = os.path.splitext(image.image.name)[1]
+                with image.image.open('rb') as f:
+                    zf.writestr(f'{slug}_{i}{ext}', f.read())
+        response: HttpResponse = HttpResponse(buffer.getvalue(), content_type='application/zip')
+        response['Content-Disposition'] = f'attachment; filename="{slug}_images.zip"'
+        return response
 
 
 @admin.register(Partsrequired)
